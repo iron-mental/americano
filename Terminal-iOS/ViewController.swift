1f85db43abbff10177c3c8f8db29b80c4a3c7621
jeongjaein <wodls96@gmail.com>
2020-08-31 19:49:16 +0900

    initial commit :(

---
+++ Terminal-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/08/31.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
