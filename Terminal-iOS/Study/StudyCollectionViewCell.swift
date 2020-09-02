f4b6d7f0145840990a27361d171a33d2c2707c17
s-doris <swdoriz@gmail.com>
2020-09-02 16:00:46 +0900

    feat : swiftlint 규칙 추가

---
+++ Terminal-iOS/Study/StudyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  StudyCollectionViewCell.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/09/02.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class StudyCollectionViewCell: UICollectionViewCell {
+    
+}
