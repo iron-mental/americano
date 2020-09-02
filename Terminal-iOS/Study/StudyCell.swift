3e4e63bc333d7db95bb8b8a39c98e4cad915e433
s-doris <swdoriz@gmail.com>
2020-09-02 23:41:31 +0900

    feat : 스터디 카테고리 뷰(임시데이터)

---
+++ Terminal-iOS/Study/StudyCell.swift
@@ -0,0 +1,45 @@
+//
+//  StudyCollectionViewCell.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/09/02.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+import Then
+
+class StudyCell: UICollectionViewCell {
+    
+    let imageView = UIImageView()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func attribute() {
+        imageView.do {
+            $0.contentMode = .scaleAspectFill
+            $0.clipsToBounds = true
+        }
+    }
+    
+    private func layout() {
+        addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    }
+}
--- Terminal-iOS/Study/StudyViewController.swift
+++ Terminal-iOS/Study/StudyViewController.swift
@@ -7,17 +7,63 @@
 //
 
 import UIKit
+import Then
 
 class StudyViewController: UIViewController {
+    
+    var temp = ["swift","android","tensorflow","node","frontend","jpark","swift","android","tensorflow","node","frontend","jpark"]
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        attirbute()
+        layout()
+        view.backgroundColor = .white
+        title = "스터디"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(createStudy))
+    }
+    
+    func attirbute() {
+        collectionView.do {
+            $0.register(StudyCell.self, forCellWithReuseIdentifier: "cell")
+            $0.delegate = self
+            $0.dataSource = self
+            $0.backgroundColor = .white
+        }
+    }
+    
+    func layout() {
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
     }
     
     @objc func createStudy() {
         let view = CreateStudyViewController()
         navigationController?.pushViewController(view, animated: true)
+    }
+}
+
+extension StudyViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width / 2.3, height: collectionView.frame.width / 3)
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return temp.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! StudyCell
+        cell.layer.cornerRadius = 10
+        cell.imageView.image = UIImage(named: temp[indexPath.row])
+        return cell
     }
 }
