079833c98b36ba3812097817a67c8fa55d360457
s-doris <swdoriz@gmail.com>
2020-09-11 22:25:52 +0900

    feat : Study 구성요소 추가

---
+++ Terminal-iOS/Study/Study.swift
@@ -0,0 +1,18 @@
+//
+//  Study.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/09/11.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+struct Study {
+    var title: String
+    var descript: String
+    var location: String
+    var date: String
+    var numberOfMember: String
+    var titleImage: UIImage
+}
