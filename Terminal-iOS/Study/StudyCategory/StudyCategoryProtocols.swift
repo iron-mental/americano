d16424abdc2fe00a638b545d3f0d09d5305c283a
s-doris <swdoriz@gmail.com>
2020-09-15 17:01:16 +0900

    feat : 스터디 카테고리 목록 프로토콜 선언

--- Terminal-iOS/Study/StudyCategory/StudyCategoryProtocols.swift
+++ Terminal-iOS/Study/StudyCategory/StudyCategoryProtocols.swift
@@ -6,4 +6,69 @@
 //  Copyright © 2020 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol StudyCategoryViewProtocol: class {
+    var presenter: StudyCategoryPresenterProtocol { get set }
+    
+    // PRESENT -> VIEW
+    func showCategoryList(with category: Category)
+    func showError()
+    func showLoading()
+    func hideLoading()
+}
+
+protocol StudyCategoryWireFrameProtocol: class {
+    static func createStudyCategory() -> UIViewController
+    
+    // PRESENTER -> WIREFRAME
+    func presentStudyListScreen(from view: StudyCategoryViewProtocol, forCategory category: Category)
+}
+
+protocol StudyCategoryPresenterProtocol: class {
+    var view: StudyCategoryViewProtocol? { get set }
+    var interactor: StudyCategoryInteractorInputProtocol? { get set }
+    var wireFrame: StudyCategoryWireFrameProtocol? { get set }
+    
+    // VIEW -> PRESENTER
+    func viewDidLoad()
+//    func showStudyListDetail(forPost post: Study)
+}
+
+protocol StudyCategoryInteractorOutputProtocol: class {
+    //INTERACTOR -> PRESENTER
+    func didRetrieveCategories(_ categories: [Category])
+    func onError()
+}
+
+protocol StudyCategoryInteractorInputProtocol: class {
+    var presenter: StudyCategoryInteractorOutputProtocol? { get set }
+    var localDatamanager: StudyCategoryLocalDataManagerInputProtocol? { get set }
+    var remoteDatamanager: StudyCategoryRemoteDataManagerInputProtocol? { get set }
+    
+    // PRESENTER -> INTERACTOR
+    func retrievePostList()
+}
+
+protocol StudyCategoryDataManagerInputProtocol: class {
+    // INTERACTOR -> DATAMANAGER
+}
+
+protocol StudyCategoryRemoteDataManagerInputProtocol: class {
+    var remoteRequestHandler: StudyCategoryRemoteDataManagerOutputProtocol? { get set }
+    
+    // INTERACTOR -> REMOTEDATAMANAGER
+    func retrievePostList()
+}
+
+protocol StudyCategoryRemoteDataManagerOutputProtocol: class {
+    // REMOTEDATAMANAGER -> INTERACTOR
+       func onCategoriesRetrieved(_ categories: [Category])
+       func onError()
+}
+
+protocol StudyCategoryLocalDataManagerInputProtocol: class {
+     // INTERACTOR -> LOCALDATAMANAGER
+    func retrieveStudyCategory() throws -> [Category]
+//    func savePost(id: Int, title: String, imageUrl: String, thumbImageUrl: String) throws
+}
