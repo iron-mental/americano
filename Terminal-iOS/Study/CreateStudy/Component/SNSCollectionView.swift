0a6bd1b314dab2a17cdda07717123bfe8f0ad23d
jeongjaein <wodls96@gmail.com>
2020-09-16 20:45:15 +0900

    feat : sns입력 컬렉션뷰 컴포넌트화

---
+++ Terminal-iOS/Study/CreateStudy/Component/SNSCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  SNSCollectionView.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/09/16.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class SNSCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
