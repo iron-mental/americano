e5255cb2e59ab261f3ed06b56b2b2a9cca6345fa
jeongjaein <wodls96@gmail.com>
2020-09-25 21:29:28 +0900

    refactor : CreateStudyInteractor snsinput 관련 func 추가

--- Terminal-iOS/Study/CreateStudy/CreateStudyInteractor.swift
+++ Terminal-iOS/Study/CreateStudy/CreateStudyInteractor.swift
@@ -10,4 +10,30 @@ import UIKit
 
 class CreateStudyInteractor: CreateStudyInteractorProtocols {
     var presenter: CreateStudyPresenterProtocols?
+    var createStudyRemoteDataManager: CreateStudyRemoteDataManagerProtocols?
+    
+    func searchNotionID(id: String?) {
+        if let userInput = id {
+            if let result = createStudyRemoteDataManager?.getNotionValid(id: userInput) {
+                presenter?.showNotionValidResult(result: result)
+            }
+        }
+        
+    }
+    
+    func searchEvernoteURL(url: String?) {
+        if let userInput = url {
+            if let result = createStudyRemoteDataManager?.getEvernoteValid(url: userInput) {
+                presenter?.showEvernoteValidResult(result: result)
+            }
+        }
+    }
+    
+    func searchWebURL(url: String?) {
+        if let userInput = url {
+            if let result = createStudyRemoteDataManager?.getWebValid(url: userInput) {
+                presenter?.showWebValidResult(result: (result))
+            }
+        }
+    }
 }
