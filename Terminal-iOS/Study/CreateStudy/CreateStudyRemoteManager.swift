58553e66ec8deccb65d2e1b66af41f1a99259e7b
jeongjaein <wodls96@gmail.com>
2020-09-25 21:24:31 +0900

    feat : add CreateStudyRemoteManager

---
+++ Terminal-iOS/Study/CreateStudy/CreateStudyRemoteManager.swift
@@ -0,0 +1,24 @@
+//
+//  CreateStudyRemoteManager.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/09/25.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class CreateStudyRemoteManager: CreateStudyRemoteDataManagerProtocols {
+    func getNotionValid(id: String?) -> Bool {
+        
+        return true
+    }
+    
+    func getEvernoteValid(url: String?) -> Bool{
+        return true
+    }
+    
+    func getWebValid(url: String?) -> Bool{
+        return true
+    }
+}
