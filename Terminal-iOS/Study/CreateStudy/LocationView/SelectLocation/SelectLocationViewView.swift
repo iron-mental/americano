dade557ee77c751defad881438b177017513b05d
jeongjaein <wodls96@gmail.com>
2020-10-06 18:32:06 +0900

    feat : SelectLocationView 네이버 맵 추가

--- Terminal-iOS/Study/CreateStudy/LocationView/SelectLocation/SelectLocationViewView.swift
+++ Terminal-iOS/Study/CreateStudy/LocationView/SelectLocation/SelectLocationViewView.swift
@@ -12,8 +12,25 @@ import NMapsMap
 class SelectLocationView: UIViewController {
     var presenter: SelectLocationPresenterProtocols?
     
+    var nmapFView = NMFMapView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        attribute()
+        layout()
+    }
+    
+    func attribute() {
+        nmapFView = NMFMapView(frame: view.frame)
+        nmapFView.do {
+            $0.mapType = .basic
+            $0.symbolScale = 0.7
+            $0.positionMode = .direction
+        }
+    }
+    
+    func layout() {
+        view.addSubview(nmapFView)
     }
 }
 extension SelectLocationView: SelectLocationViewProtocols {
