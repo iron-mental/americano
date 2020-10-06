a584e8893bd97c1576c8614de8f92ebe0c289758
jeongjaein <wodls96@gmail.com>
2020-10-06 15:43:57 +0900

    feat : presenter에 wireFrame 할당

--- Terminal-iOS/Study/CreateStudy/CreateStudyWireFrame.swift
+++ Terminal-iOS/Study/CreateStudy/CreateStudyWireFrame.swift
@@ -15,12 +15,15 @@ class CreateStudyWireFrame: CreateStudyWireFrameProtocols {
         let presenter = CreateStudyPresenter()
         let interactor = CreateStudyInteractor()
         let remoteDataManager = CreateStudyRemoteManager()
+        let wireFrame = CreateStudyWireFrame()
         
         view.presenter = presenter
         view.selectedCategory = category
         
         presenter.view = view
         presenter.interactor = interactor
+        
+        presenter.wireFrame = wireFrame
         
         interactor.presenter = presenter
         interactor.createStudyRemoteDataManager = remoteDataManager
@@ -31,7 +34,11 @@ class CreateStudyWireFrame: CreateStudyWireFrameProtocols {
     }
     
     func goToSelectLocation(view: UIViewController) {
-        let view =  SelectLocationWireFrame.selectLocationViewModul()
+
+        let selectLocationView =  SelectLocationWireFrame.selectLocationViewModul()
+        
+        //modal의 형태를 추후에 정하구요 dismiss 시켜주는 것 만으로 다시 원래 플로우인 스터디 생성 플로우로 돌아가게 하면 깔끔 할 것 같은 느낌
+        view.present(selectLocationView, animated: true, completion: nil)
         //받아온 리턴값 view를 
     }
 }
