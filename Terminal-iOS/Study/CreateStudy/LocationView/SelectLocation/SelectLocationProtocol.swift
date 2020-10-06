2814b862dff6ec083bda03464798e3da653b4c9b
jeongjaein <wodls96@gmail.com>
2020-10-06 17:14:31 +0900

    feat : SelectLocationProtocol 앱내 의사소통 정의

--- Terminal-iOS/Study/CreateStudy/LocationView/SelectLocation/SelectLocationProtocol.swift
+++ Terminal-iOS/Study/CreateStudy/LocationView/SelectLocation/SelectLocationProtocol.swift
@@ -7,29 +7,44 @@
 //
 
 import UIKit
+import NMapsMap
 
 protocol SelectLocationViewProtocol: class {
     var presenter: SelectLocationPresenterProtocol { get set }
+    
+    //PRESENTER -> VIEW
+    func setViewWithResult(latLng: NMGLatLng, address: String)
 }
 
 protocol SelectLocationInteractorProtocol: class {
     var presenter: SelectLocationPresenterProtocol { get set }
     var remoteDataManager: SelectLocationRemoteDataManagerProtocol { get set }
     var localDataManager: SelectLocationLocalDataManagerProtocol { get set }
+    
+    //PRESENTER -> INTERACTOR
+    func searchAddress(lat: Double?, Lng: Double?)
 }
 
 protocol SelectLocationPresenterProtocol: class {
     var view: SelectLocationViewProtocol { get set }
     var interactor: SelectLocationInteractorProtocol { get set }
     var wireFrame: SelectLocationWireFrameProtocol { get set }
+    
+    //VIEW -> PRESENTER
+    func getAddress(lat: Double?, Lng: Double?)
+    
+    //INTERACTOR -> PRESENTER
+    func getAddressResult(latLng: NMGLatLng, address: String)
 }
 
 protocol SelectLocationRemoteDataManagerProtocol: class {
-
+    //INTERACTOR -> REMOTEDATAMANAGER
+    func getAddressWithLatLng(latLng: NMGLatLng?) -> (NMGLatLng, String)
 }
 
 protocol SelectLocationLocalDataManagerProtocol: class {
-
+    //INTERACTOR -> LOCALDATAMANAGER
+    func getCurrentLocation() ->(NMGLatLng)
 }
 
 protocol SelectLocationWireFrameProtocol: class {
