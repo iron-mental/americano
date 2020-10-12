2c08a1625bc5c01e041851a4606ded3600319c08
jeongjaein <wodls96@gmail.com>
2020-10-12 14:58:43 +0900

    feat : MyStudyMainView more버튼, 액션 추가

--- Terminal-iOS/MyStudy/Main/MyStudyMainView.swift
+++ Terminal-iOS/MyStudy/Main/MyStudyMainView.swift
@@ -11,9 +11,35 @@ import UIKit
 class MyStudyMainView: UIViewController {
     var presenter: MyStudyMainPresenterProtocol?
     
+    var moreButton: UIBarButtonItem?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        attribute()
+        layout()
+    }
+    
+    func attribute() {
+        view.do {
+            $0.backgroundColor = UIColor.appColor(.terminalBackground)
+        }
+        self.do {
+            $0.title = "내 스터디"
+        }
+        moreButton = UIBarButtonItem(title: "왜이래", style: .plain, target: self, action: #selector(moreButtonAction(_ :)))
+        moreButton?.do {
+            //추후에 more버튼으로 교체
+            $0.image = #imageLiteral(resourceName: "Vaild")
+        }
+        
+    }
+    
+    func layout() {
+        self.navigationItem.rightBarButtonItem = moreButton
+    }
+    
+    @objc func moreButtonAction(_ sender: UIBarButtonItem) {
+        print("clicked more Button!!")
     }
 }
 
