1bfbe7d81c7d340044b3d0ca233a23584f12d298
s-doris <swdoriz@gmail.com>
2020-10-14 23:22:11 +0900

    feat : keyword collectionView cell 생성

--- Terminal-iOS/Study/SearchStudy/Component/HotKeywordCell.swift
+++ Terminal-iOS/Study/SearchStudy/Component/HotKeywordCell.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2020 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class HotKeywordCell: UICollectionViewCell {
+    
+    let keyword = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func attribute() {
+        keyword.do {
+            $0.layer.borderWidth = 1
+            $0.setTitleColor(.white, for: .normal)
+            $0.backgroundColor = UIColor.appColor(.mainColor)
+            $0.layer.cornerRadius = 10
+        }
+    }
+    
+    private func layout() {
+        addSubview(keyword)
+        keyword.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: topAnchor).isActive = true
+            $0.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        }
+    }
+}
