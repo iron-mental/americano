7515bbdd444566b6347fa4f55dda2d4622163853
jeongjaein <wodls96@gmail.com>
2020-10-22 00:31:13 +0900

    feat : loginView textField 준비

--- Terminal-iOS/Login/LoginView.swift
+++ Terminal-iOS/Login/LoginView.swift
@@ -9,9 +9,31 @@
 import UIKit
 
 class LoginView: UIViewController {
-
+    var emailTextfield = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        attribute()
+        layout()
+    }
+    
+    func attribute() {
+        emailTextfield.do {
+            $0.placeholder = "placehodler"
+            $0.backgroundColor = UIColor.appColor(.testColor)
+            $0.textColor = .gray
+            $0.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width * ( 285 / 375 ), height: UIScreen.main.bounds.height * ( 32 / 667 ))
+        }
+    }
+    
+    func layout() {
+        view.addSubview(emailTextfield)
+        
+        emailTextfield.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+            $0.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        }
     }
 }
