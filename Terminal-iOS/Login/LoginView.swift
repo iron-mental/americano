f9f77427f37ab76ea62633285fd724adcebb1f30
jeongjaein <wodls96@gmail.com>
2020-10-22 19:44:03 +0900

    feat : cancel Button 추가

--- Terminal-iOS/Login/LoginView.swift
+++ Terminal-iOS/Login/LoginView.swift
@@ -13,9 +13,11 @@ class LoginView: UIViewController {
     var nextButton = UIButton()
     var guideLabel = UILabel()
     var emailTextfield = UITextField()
+    var cancelButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTextfield.becomeFirstResponder()
         attribute()
         layout()
     }
@@ -25,7 +27,8 @@ class LoginView: UIViewController {
             $0.view.backgroundColor = UIColor.appColor(.testColor)
         }
         emailTextfield.do {
-            $0.placeholder = "youremail@youplatform.com"
+            $0.placeholder = "abc1234@terminal.com"
+            $0.font = UIFont.boldSystemFont(ofSize: 18)
         }
         closeButton.do {
             $0.setImage(#imageLiteral(resourceName: "close"), for: .normal)
@@ -41,19 +44,20 @@ class LoginView: UIViewController {
             $0.text = "이메일을\n입력해 주세요"
             $0.font = UIFont.boldSystemFont(ofSize: 24)
         }
+        cancelButton.do {
+            $0.setImage(#imageLiteral(resourceName: "cancel"), for: .normal)
+            $0.addTarget(self, action: #selector(didClickedCancelButton), for: .touchUpInside)
+        }
     }
     
     func layout() {
-        view.addSubview(emailTextfield)
-        view.addSubview(closeButton)
-        view.addSubview(nextButton)
-        view.addSubview(guideLabel)
+        [emailTextfield, closeButton, nextButton, guideLabel, cancelButton].forEach { view.addSubview($0) }
         
         emailTextfield.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-            $0.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-            $0.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * ( 285 / 375 )).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: (40/375) * UIScreen.main.bounds.width).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * ( 235 / 375 )).isActive = true
             $0.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * ( 32 / 667 )).isActive = true
         }
         closeButton.do {
@@ -77,6 +81,13 @@ class LoginView: UIViewController {
             $0.widthAnchor.constraint(equalToConstant: (137/375) * UIScreen.main.bounds.width).isActive = true
             $0.heightAnchor.constraint(equalToConstant: (93/667) * UIScreen.main.bounds.height).isActive = true
         }
+        cancelButton.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+            $0.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 50).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        }
     }
     
     @objc func didClickedCloseButon() {
@@ -85,5 +96,8 @@ class LoginView: UIViewController {
     
     @objc func didClickedNextButton() {
         print("clicked NextButton!")
+    }
+    @objc func didClickedCancelButton() {
+        emailTextfield.text = ""
     }
 }
