f3bb77c81b8bb12dfdeca2b3358eded61030950b
s-doris <swdoriz@gmail.com>
2020-10-25 18:15:13 +0900

    refactor : layout error resolve

--- Terminal-iOS/Set/ProfileModify/ProfileModifyView.swift
+++ Terminal-iOS/Set/ProfileModify/ProfileModifyView.swift
@@ -28,25 +28,13 @@ class ProfileModifyView: UIViewController {
     let sns = SNSView().then {
         $0.backgroundColor = .blue
     }
-    
-    let projectStack = UIStackView()
-    
-    let snsBackground = UIView().then {
-        $0.backgroundColor = .cyan
+    let email = EmailView().then {
+        $0.backgroundColor = .yellow
     }
-    let snsLabel = UILabel()
-    
-    let emailBackground = UIView().then {
-        $0.backgroundColor = .brown
-    }
-    let emailLabel = UILabel()
-    let email = UILabel()
-    
-    let locationBackground = UIView().then {
+    let location = LocationView().then {
         $0.backgroundColor = .blue
     }
-    let locationLabel = UILabel()
-    let location = UILabel()
+    let projectStack = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,47 +50,11 @@ class ProfileModifyView: UIViewController {
             $0.view.backgroundColor = UIColor.appColor(.terminalBackground)
             $0.navigationItem.rightBarButtonItem = modifyBtn
         }
-//        projectStack.do {
-//            $0.axis = .vertical
-//            $0.alignment = .leading
-//            $0.distribution = .fillProportionally
-//            $0.backgroundColor = .blue
-//            $0.addArrangedSubview(projectLabel)
-//            $0.addArrangedSubview(projectTitle)
-//            $0.addArrangedSubview(projectDescript)
-//        }
-//        
-        snsLabel.do {
-            $0.text = "SNS"
-            $0.textColor = .white
-            $0.dynamicFont(fontSize: 12, weight: .regular)
-        }
-        emailLabel.do {
-            $0.text = "Email"
-            $0.textColor = .black
-        }
-        email.do {
-            $0.text = "jerry@gmail.com"
-            $0.textColor = .black
-        }
-        locationLabel.do {
-            $0.text = "활동지역"
-            $0.textColor = .black
-        }
-        location.do {
-            $0.text = "서울 마포구 주민입니다. 서울 모든 곳 가능합니다."
-            $0.textColor = .black
-        }
-
     }
     
     func layout() {
         view.addSubview(scrollView)
-        scrollView.addSubview(backgroundView)
-        backgroundView.addSubview(profile)
-        backgroundView.addSubview(carrer)
-        backgroundView.addSubview(project)
-        backgroundView.addSubview(sns)
+        [profile, carrer, project, sns, email, location].forEach { scrollView.addSubview($0) }
         
         // 스크롤뷰 오토레이아웃
         scrollView.do {
@@ -112,42 +64,56 @@ class ProfileModifyView: UIViewController {
             $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
             $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         }
-        backgroundView.do {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
-            $0.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
-            $0.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
-            $0.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
-            $0.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
-            $0.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
-        }
+//        backgroundView.do {
+//            $0.translatesAutoresizingMaskIntoConstraints = false
+//            $0.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+//            $0.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+//            $0.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+//            $0.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+//            $0.heightAnchor.constraint(equalToConstant: 800).isActive = true
+//
+//        }
         profile.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: backgroundView.topAnchor).isActive = true
-            $0.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor).isActive = true
-            $0.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor).isActive = true
+            $0.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
             $0.heightAnchor.constraint(equalTo: profile.heightAnchor).isActive = true
         }
         carrer.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: profile.bottomAnchor).isActive = true
-            $0.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor).isActive = true
-            $0.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
             $0.heightAnchor.constraint(equalTo: carrer.heightAnchor).isActive = true
         }
         project.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: carrer.bottomAnchor).isActive = true
-            $0.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor).isActive = true
-            $0.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
             $0.heightAnchor.constraint(equalTo: project.heightAnchor).isActive = true
         }
         sns.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: project.bottomAnchor).isActive = true
-            $0.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor).isActive = true
-            $0.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
             $0.heightAnchor.constraint(equalTo: sns.heightAnchor).isActive = true
+        }
+        email.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: sns.bottomAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            $0.heightAnchor.constraint(equalTo: email.heightAnchor).isActive = true
+        }
+        location.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: email.bottomAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            $0.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
         }
     }
 }
