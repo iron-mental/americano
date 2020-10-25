cbf1b8b5bedf376329b9308038d2c326306b2b86
s-doris <swdoriz@gmail.com>
2020-10-25 16:51:02 +0900

    refactor : autolayout bottom constraint

--- Terminal-iOS/Set/ProfileModify/Component/CarrerView.swift
+++ Terminal-iOS/Set/ProfileModify/Component/CarrerView.swift
@@ -10,19 +10,56 @@ import UIKit
 
 class CarrerView: UIView {
     
+    let careerLabel = UILabel()
+    let careerTitle = UILabel()
+    let careerDescript = UILabel()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         attribute()
         layout()
     }
    
-    
     func attribute() {
-        
+        careerLabel.do {
+            $0.text = "경력"
+            $0.textColor = .black
+        }
+        careerTitle.do {
+            $0.text = "OO대학교 4학년 재학중"
+            $0.textColor = .black
+            $0.dynamicFont(fontSize: 16, weight: .bold)
+        }
+        careerDescript.do {
+            $0.text = "경력에 대한 짧은 소개가 들어가는 중입니다. 경력에 대한 짧은 소개가 들어가는 중입니다. 경력에 대한 짧은 소개가 들어가는 중입니다.경력에 대한 짧은 소개가 들어가는 중입니다. 경력에 대한 짧은 소개가 들어가는 중입니다. 경력에 대한 짧은 소개가 들어가는 중입니다."
+            $0.numberOfLines = 0
+            $0.textColor = .black
+            $0.dynamicFont(fontSize: 14, weight: .regular)
+        }
     }
     
     func layout() {
+        addSubview(careerLabel)
+        addSubview(careerTitle)
+        addSubview(careerDescript)
         
+        careerLabel.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: self.topAnchor, constant: 4).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+        }
+        careerTitle.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: self.careerLabel.bottomAnchor, constant: 4).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+        }
+        careerDescript.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: careerTitle.bottomAnchor, constant: 4).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+            $0.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
+            $0.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
+        }
     }
     
     required init?(coder: NSCoder) {
