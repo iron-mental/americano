de7b5141d20ed2b39a4a0ad32cc23ed21623319d
jeongjaein <wodls96@gmail.com>
2020-10-25 16:41:30 +0900

    feat : Terminal 클래스 생성

---
+++ Terminal-iOS/Entity/Terminal.swift
@@ -0,0 +1,21 @@
+//
+//  Terminal.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/10/25.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class Terminal {
+    static let screenSize = UIScreen.main.bounds
+
+    static func convertHeigt(value: CGFloat) -> CGFloat{
+        return screenSize.height * (value / 667)
+    }
+    
+    static func convertWidth(value: CGFloat) -> CGFloat {
+        return screenSize.width * (value / 375)
+    }
+}
---
+++ Terminal-iOS/Login/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/10/25.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
