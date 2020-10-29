8b9c6b5a8872174a65e5eb9662cf58edd9b176c1
s-doris <swdoriz@gmail.com>
2020-10-29 15:07:02 +0900

    feat : Notification Cell attribute 및 layout 선언

--- Terminal-iOS/MyStudy/Notification/Component/NotificationCell.swift
+++ Terminal-iOS/MyStudy/Notification/Component/NotificationCell.swift
@@ -21,10 +21,30 @@ class NotificationCell: UITableViewCell {
     }
     
     func attribute() {
-        
+        title.do {
+            $0.textColor = .white
+            $0.dynamicFont(fontSize: 16, weight: .semibold)
+            $0.numberOfLines = 1
+        }
+        explain.do {
+            $0.textColor = .white
+            $0.dynamicFont(fontSize: 14, weight: .regular)
+            $0.numberOfLines = 1
+        }
     }
     func layout() {
-        
+        title.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: self.topAnchor, constant: Terminal.convertHeigt(value: 13)).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Terminal.convertWidth(value: 20)).isActive = true
+        }
+        explain.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: title.bottomAnchor, constant: Terminal.convertHeigt(value: 5)).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Terminal.convertWidth(value: 20)).isActive = true
+            $0.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Terminal.convertWidth(value: 100)).isActive = true
+            $0.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -Terminal.convertHeigt(value: 5)).isActive = true
+        }
     }
     
     required init?(coder: NSCoder) {
