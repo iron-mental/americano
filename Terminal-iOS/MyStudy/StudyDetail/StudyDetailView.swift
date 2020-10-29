609c3dfb2d8e49aeddcfe5f6d2c910782db3bf66
s-doris <swdoriz@gmail.com>
2020-10-29 19:54:39 +0900

    feat : 상단탭 UISegmentControl 사용

--- Terminal-iOS/MyStudy/StudyDetail/StudyDetailView.swift
+++ Terminal-iOS/MyStudy/StudyDetail/StudyDetailView.swift
@@ -11,36 +11,65 @@ import UIKit
 class StudyDetailView: UIViewController {
     let state: [String] = ["공지사항", "스터디 정보", "채팅"]
     
-    let tabButton: UICollectionView = {
+    lazy var tabButton: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
         return view
     }()
     
+    lazy var tabSege = UISegmentedControl(items: state)
+    lazy var selectedUnderLine = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
         attribute()
         layout()
     }
     
     func attribute() {
         tabButton.do {
+            $0.backgroundColor = .black
             $0.delegate = self
             $0.dataSource = self
             $0.register(tabButtonCell.self, forCellWithReuseIdentifier: tabButtonCell.cellID)
+        }
+        tabSege.do {
+            $0.setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.gray], for: .normal)
+            $0.setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 16), .foregroundColor: UIColor.white], for: .selected)
+            $0.backgroundColor = .clear
+            $0.tintColor = .white
+            $0.selectedSegmentTintColor = .clear
+        }
+        selectedUnderLine.do {
+            $0.backgroundColor = .white
         }
     }
     
     func layout() {
         view.addSubview(tabButton)
+        view.addSubview(tabSege)
+        view.addSubview(selectedUnderLine)
         
         tabButton.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
             $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
             $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-            $0.heightAnchor.constraint(equalToConstant: Terminal.convertHeigt(value: 40)).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: Terminal.convertHeigt(value: 38)).isActive = true
+        }
+        tabSege.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: tabButton.bottomAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: Terminal.convertHeigt(value: 38)).isActive = true
+        }
+        selectedUnderLine.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.bottomAnchor.constraint(equalTo: tabSege.bottomAnchor).isActive = true
+            $0.centerXAnchor.constraint(equalTo: tabSege.centerXAnchor, constant: -view.frame.width / 3).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 3).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 50).isActive = true
         }
     }
 }
