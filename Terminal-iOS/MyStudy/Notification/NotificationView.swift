4178812fe6e368cafee87c49deee40769b88b4ef
s-doris <swdoriz@gmail.com>
2020-10-29 14:50:48 +0900

    feat : NotificationView init

---
+++ Terminal-iOS/MyStudy/Notification/NotificationView.swift
@@ -0,0 +1,36 @@
+//
+//  NotificationView.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/10/29.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class NotificationView: UIViewController {
+    let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        attribute()
+        layout()
+    }
+    
+    func attribute() {
+        
+    }
+    func layout() {
+        
+    }
+}
+
+extension NotificationView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+}
