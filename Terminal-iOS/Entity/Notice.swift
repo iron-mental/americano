4a06e04f5586c80468f99a242ea3d7d717e5e008
s-doris <swdoriz@gmail.com>
2020-11-01 16:00:56 +0900

    feat : pageView init

---
+++ Terminal-iOS/Entity/Notice.swift
@@ -0,0 +1,9 @@
+//
+//  Notice.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/10/31.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import Foundation
---
+++ Terminal-iOS/MyStudy/StudyDetail/PageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PageViewController.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/11/01.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    let VCArr: [UIViewController] = [ NoticeView(),
+                                      TempChatView()]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+        self.dataSource = self
+        
+        if let firstVC = VCArr.first{
+            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
+    }
+}
+
+extension PageViewController: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = VCArr.firstIndex(of: viewController), index > 0 else { return nil }
+        let previousIndex = index - 1
+        return VCArr[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = VCArr.firstIndex(of: viewController), index < (VCArr.count - 1) else { return nil }
+        let nextIndex = index + 1
+        return VCArr[nextIndex]
+    }
+}
