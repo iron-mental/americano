73c0dc98209ae86aa3665ededf690fed57c0b2f3
jeongjaein <wodls96@gmail.com>
2020-11-08 14:27:52 +0900

    머지 준비

--- Terminal-iOS/MyStudy/Main/MyStudyMainView.swift
+++ Terminal-iOS/MyStudy/Main/MyStudyMainView.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum MyStudyMainViewState {
+    case edit
+    case normal
+}
+
 class MyStudyMainView: UIViewController {
     var presenter: MyStudyMainPresenterProtocol?
     
@@ -16,9 +21,10 @@ class MyStudyMainView: UIViewController {
     var alarmButton = badgeBarButtonItem()
     var tempButton: UIBarButtonItem?
     var rightBarButtomItem: UIBarButtonItem?
-    
+    var state: MyStudyMainViewState = .normal
     //alarmbutton 쇼잉을 위한 임시 변수!! 곧 삭제됩니다.
     var tempCountForBadge = 0
+    var checkedArray: [Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,9 +46,6 @@ class MyStudyMainView: UIViewController {
             $0.title = "내 스터디"
             $0.navigationItem.rightBarButtonItems = [moreButton!, alarmButton, tempButton!]
             $0.navigationController?.navigationBar.backgroundColor = UIColor.appColor(.testColor)
-            
-            //임시로 이렇게 처리
-//            $0.navigationController?.navigationBar.prefersLargeTitles = true
         }
         tableView.do {
             $0.backgroundColor = UIColor.appColor(.testColor)
@@ -69,19 +72,30 @@ class MyStudyMainView: UIViewController {
     
     @objc func moreButtonAction(_ sender: UIBarButtonItem) {
         let alert =  UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        let edit =  UIAlertAction(title: "스터디 편집", style: .default) {_ in }
+        let edit =  UIAlertAction(title: "스터디 편집", style: .default) {(action) in self.editButtonAction()}
         let temp =  UIAlertAction(title: "여긴뭐들어갑니까", style: .default) {_ in }
         let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
         [edit,temp,cancel].forEach {
             alert.addAction($0)
         }
         present(alert, animated: true, completion: nil)
     }
     
+    @objc func editButtonAction() {
+        self.state = .edit
+        navigationItem.rightBarButtonItems?.forEach {
+            $0.isEnabled = false
+        }
+        tableView.reloadData()
+    }
     @objc func alarmButtonAction(_ sender: UIBarButtonItem) {
-//        alarmButton.badgeLabel.isHidden = false
-//        tempCountForBadge += 1
-//        alarmButton.badgeLabel.text = "\(tempCountForBadge)"
+        self.state = .normal
+        navigationItem.rightBarButtonItems?.forEach {
+            $0.isEnabled = true
+            $0.tintColor = UIColor.systemBackground
+        }
+        tableView.reloadData()
         let view = NotificationView()
         self.navigationController?.pushViewController(view, animated: true)
     }
@@ -95,13 +109,10 @@ class MyStudyMainView: UIViewController {
     }
 }
 
-extension MyStudyMainView: MyStudyMainViewProtocol {
-    
-}
-
 extension MyStudyMainView: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        
+        return self.state == .normal ? 1 : 2
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: MyStudyMainTableViewCell.identifier) as! MyStudyMainTableViewCell
@@ -110,4 +121,12 @@ extension MyStudyMainView: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return (91.7/667) * view.bounds.height
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        checkedArray.append(indexPath.row)
+    }
 }
+
+extension MyStudyMainView: MyStudyMainViewProtocol {
+    
+}
+
