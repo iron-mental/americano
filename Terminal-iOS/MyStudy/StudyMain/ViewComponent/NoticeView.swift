4b4a29dfd89cb9ce73112672559018d8335b4d2c
s-doris <swdoriz@gmail.com>
2020-11-09 01:18:47 +0900

    feat : 공지사항 필독, 일반 정리

--- Terminal-iOS/MyStudy/StudyMain/ViewComponent/NoticeView.swift
+++ Terminal-iOS/MyStudy/StudyMain/ViewComponent/NoticeView.swift
@@ -11,16 +11,35 @@ import UIKit
 class NoticeView: UIViewController {
     let noticeTitleText = ["모임 진행시 가이드 라인입니다", "모임 진행시 가이드 라인입니다", "모임 진행시 가이드 라인입니다", "모임 진행시 가이드 라인입니다", "모임 진행시 가이드 라인입니다","모임 진행시 가이드 라인입니다", "모임 진행시 가이드 라인입니다", "모임 진행시 가이드 라인입니다", "모임 진행시 가이드 라인입니다", "모임 진행시 가이드 라인입니다"]
     
+    var noticeList: [Notice] = [Notice(title: "가이드 라인입니다가이드 라인입니다가이드 라인입니다가이드 라인입니다가이드 라인입니다가이드 라인입니다", contents: "첫번모임 진행시 가이드 라인입니다모임 진행시 가이드 라인입니다모임 진행시 가이드 라인입니다모임 진행시 가이드 라인입니다모임 진행시 가이드 라인입니다모임 진행시 가이드 라인입니다모임 진행시 가이드 라인입니다모임 진행시 가이드 라인입니다모임 진행시 가이드 라인입니다모임 진행시 가이드 라인입니다모임 진행시 가이드 라인입니다째", pinned: true),
+                                Notice(title: "가이드 라인입니다", contents: "두번쨰", pinned: false),
+                                Notice(title: "가이드 라인입니다", contents: "세번째", pinned: false),
+                                Notice(title: "가이드 라인입니다", contents: "네번째", pinned: true),
+                                Notice(title: "가이드 라인입니다", contents: "다섯번째", pinned: false),
+                                Notice(title: "가이드 라인입니다", contents: "여섯번째", pinned: false),
+                                Notice(title: "가이드 라인입니다", contents: "일곱번째", pinned: true),
+                                Notice(title: "가이드 라인입니다", contents: "여덟번째", pinned: false)]
+    var pinnedNotiArr: [Notice] = []
+    var notiArr: [Notice] = []
     lazy var notice = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        sorted()
         attribute()
         layout()
     }
+    
+    func sorted() {
+        pinnedNotiArr = noticeList.filter { $0.pinned }
+        notiArr = noticeList.filter { !$0.pinned }
+        print(pinnedNotiArr)
+        print(notiArr)
+    }
     func attribute() {
         notice.do {
-            $0.register(NoticeCell.self, forCellReuseIdentifier: NoticeCell.cellID)
+            $0.register(NoticeCell.self, forCellReuseIdentifier: NoticeCell.noticeCellID)
+            $0.register(PinnedNoticeCell.self, forCellReuseIdentifier: PinnedNoticeCell.pinnedNoticeCellID)
             $0.delegate = self
             $0.dataSource = self
             $0.bounces = false
@@ -41,13 +60,31 @@ class NoticeView: UIViewController {
 }
 
 extension NoticeView: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return noticeTitleText.count
+        if section == 0 {
+            return pinnedNotiArr.count
+        } else if section == 1 {
+            return notiArr.count
+        } else {
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = notice.dequeueReusableCell(withIdentifier: NoticeCell.cellID, for: indexPath) as! NoticeCell
-        cell.noticeTitle.text = noticeTitleText[indexPath.row]
-        return cell
+        let noticeCell = notice.dequeueReusableCell(withIdentifier: NoticeCell.noticeCellID, for: indexPath) as! NoticeCell
+        
+        if indexPath.section == 0 {
+            noticeCell.noticeLabel.text = "필독"
+            noticeCell.noticeBackground.backgroundColor = UIColor.appColor(.pinnedNoticeColor)
+            noticeCell.setData(pinnedNotiArr[indexPath.row])
+        } else if indexPath.section == 1 {
+            noticeCell.setData(notiArr[indexPath.row])
+        }
+        return noticeCell
     }
 }
