2bb2b259c71cb853dad657f84c496c949d6bdbd1
s-doris <swdoriz@gmail.com>
2020-11-08 15:25:10 +0900

    feat : notice label background init

--- Terminal-iOS/MyStudy/StudyMain/Component/NoticeCell.swift
+++ Terminal-iOS/MyStudy/StudyMain/Component/NoticeCell.swift
@@ -11,6 +11,7 @@ import UIKit
 class NoticeCell: UITableViewCell {
     static let cellID = "NoticeCellID"
     
+    lazy var noticeBackground = UIView()
     lazy var noticeLabel = UILabel()
     lazy var noticeTitle = UILabel()
     lazy var date = UILabel()
@@ -22,10 +23,13 @@ class NoticeCell: UITableViewCell {
     }
     
     func attribute() {
-        noticeLabel.do {
+        noticeBackground.do {
             $0.backgroundColor = UIColor.appColor(.noticeColor)
+            $0.layer.cornerRadius = 5
+        }
+        noticeLabel.do {
             $0.dynamicFont(fontSize: 12, weight: .medium)
-            $0.text = "공지사항"
+            $0.text = "공지"
             $0.textAlignment = .center
             $0.textColor = .white
             $0.clipsToBounds = true
@@ -41,20 +45,26 @@ class NoticeCell: UITableViewCell {
         }
     }
     func layout() {
-        addSubview(noticeLabel)
+        addSubview(noticeBackground)
+        noticeBackground.addSubview(noticeLabel)
         addSubview(noticeTitle)
         addSubview(date)
+        noticeBackground.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: self.topAnchor, constant: Terminal.convertHeigt(value: 9)).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Terminal.convertHeigt(value: 13)).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: Terminal.convertHeigt(value: 41)).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: Terminal.convertHeigt(value: 20)).isActive = true
+        }
         noticeLabel.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: self.topAnchor, constant: Terminal.convertHeigt(value: 10)).isActive = true
-            $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Terminal.convertHeigt(value: 13)).isActive = true
-            $0.widthAnchor.constraint(equalToConstant: Terminal.convertWidth(value: 50)).isActive = true
-            $0.heightAnchor.constraint(equalToConstant: Terminal.convertHeigt(value: 25)).isActive = true
+            $0.centerXAnchor.constraint(equalTo: noticeBackground.centerXAnchor).isActive = true
+            $0.centerYAnchor.constraint(equalTo: noticeBackground.centerYAnchor).isActive = true
         }
         noticeTitle.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: self.topAnchor, constant: Terminal.convertHeigt(value: 10)).isActive = true
-            $0.leadingAnchor.constraint(equalTo: self.noticeLabel.trailingAnchor, constant: Terminal.convertWidth(value: 15)).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.noticeBackground.trailingAnchor, constant: Terminal.convertWidth(value: 15)).isActive = true
         }
         date.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
