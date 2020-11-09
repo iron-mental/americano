aca396b285863e73c72bffe93674033568e8c82b
s-doris <swdoriz@gmail.com>
2020-11-09 22:28:07 +0900

    feat : career area view init

--- Terminal-iOS/Set/ProfileModify/ProfileModifyView.swift
+++ Terminal-iOS/Set/ProfileModify/ProfileModifyView.swift
@@ -12,13 +12,16 @@ class ProfileModifyView: UIViewController {
     lazy var profileImage = UIImageView()
     lazy var nameModify = UITextField()
     lazy var descripModify = UITextView()
-    
+    lazy var careerLabel = UILabel()
+    lazy var careerTitleModify = UITextField()
+    lazy var careerDescriptModify = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         attribute()
         layout()
         textViewDidChange(descripModify)
+        textViewDidChange(careerDescriptModify)
     }
     
     func attribute() {
@@ -38,9 +41,30 @@ class ProfileModifyView: UIViewController {
         descripModify.do {
             $0.backgroundColor = .red
             $0.delegate = self
-            $0.font = UIFont.systemFont(ofSize: 16*1.2, weight: .regular)
+            $0.dynamicFont(size: 16, weight: .regular)
             $0.textColor = .white
             $0.sizeToFit()
+            $0.textContainer.lineFragmentPadding = 0
+            $0.textContainerInset = .zero
+        }
+        careerLabel.do {
+            $0.text = "경력"
+            $0.textColor = .white
+        }
+        careerTitleModify.do {
+            $0.text = "OO대학교 4학년 재학중"
+            $0.textColor = .white
+            $0.dynamicFont(fontSize: 16, weight: .bold)
+            $0.textAlignment = .left
+            $0.backgroundColor = .blue
+        }
+        careerDescriptModify.do {
+            $0.text = "경력에 대한 짧은 소개가 들어가는 중입니다. 경력에 대한 짧은 소개가 들어가는 중입니다. 경력에 대한 짧은 소개가 들어가는 중입니다.경력에 대한 짧은 소개가 들어가는 중입니다. 경력에 대한 짧은 소개가 들어가는 중입니다. 경력에 대한 짧은 소개가 들어가는 중입니다."
+            $0.textColor = UIColor.appColor(.profileTextColor)
+            $0.dynamicFont(size: 14, weight: .regular)
+            $0.delegate = self
+            $0.sizeToFit()
+            $0.backgroundColor = .red
             $0.textContainer.lineFragmentPadding = 0
             $0.textContainerInset = .zero
         }
@@ -50,6 +74,10 @@ class ProfileModifyView: UIViewController {
         view.addSubview(profileImage)
         view.addSubview(nameModify)
         view.addSubview(descripModify)
+        view.addSubview(careerLabel)
+        view.addSubview(careerTitleModify)
+        view.addSubview(careerDescriptModify)
+        
         profileImage.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
@@ -65,31 +93,43 @@ class ProfileModifyView: UIViewController {
         descripModify.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: nameModify.bottomAnchor, constant: 7).isActive = true
-            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
-            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25).isActive = true
             $0.heightAnchor.constraint(lessThanOrEqualToConstant: 10).isActive = true
-//            $0.heightAnchor.constraint(equalToConstant: 100).isActive = true
-//            $0.heightAnchor.constraint(equalTo: descripModify.heightAnchor).isActive = true
+        }
+        careerLabel.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: descripModify.bottomAnchor, constant: 18).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25).isActive = true
+        }
+        careerTitleModify.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: careerLabel.bottomAnchor, constant: 4).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25).isActive = true
+        }
+        careerDescriptModify.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: careerTitleModify.bottomAnchor, constant: 4).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25).isActive = true
+            $0.heightAnchor.constraint(lessThanOrEqualToConstant: 10).isActive = true
         }
     }
-
 }
 
 
 extension ProfileModifyView: UITextViewDelegate {
+    
+    //MARK: TextView Dynamic Height
     func textViewDidChange(_ textView: UITextView) {
         let size = CGSize(width: view.frame.width, height: .infinity)
-                let estimatedSize = textView.sizeThatFits(size)
-                textView.constraints.forEach { (constraint) in
-                    if constraint.firstAttribute == .height {
-                        constraint.constant = estimatedSize.height
-                    }
-                }
-//        let fixedWidth = textView.frame.size.width
-//        textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
-//        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
-//        var newFrame = textView.frame
-//        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
-//        textView.frame = newFrame
+        let estimatedSize = textView.sizeThatFits(size)
+        textView.constraints.forEach { (constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimatedSize.height
+            }
+        }
     }
 }
