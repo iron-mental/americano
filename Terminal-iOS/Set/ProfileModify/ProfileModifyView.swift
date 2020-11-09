a12b4a4ac95295c8e001af75a64bd7fc8628b741
s-doris <swdoriz@gmail.com>
2020-11-09 20:21:50 +0900

    feat : profile modify view init

--- Terminal-iOS/Set/ProfileModify/ProfileModifyView.swift
+++ Terminal-iOS/Set/ProfileModify/ProfileModifyView.swift
@@ -11,13 +11,14 @@ import UIKit
 class ProfileModifyView: UIViewController {
     lazy var profileImage = UIImageView()
     lazy var nameModify = UITextField()
-    lazy var descripModify = UITextField()
+    lazy var descripModify = UITextView()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         attribute()
         layout()
+        textViewDidChange(descripModify)
     }
     
     func attribute() {
@@ -30,10 +31,25 @@ class ProfileModifyView: UIViewController {
             $0.layer.cornerRadius = $0.frame.width / 2
             $0.clipsToBounds = true
         }
+        nameModify.do {
+            $0.font = UIFont(name: nameModify.font!.fontName, size: 20)
+            $0.dynamicFont(fontSize: 20, weight: .semibold)
+        }
+        descripModify.do {
+            $0.backgroundColor = .red
+            $0.delegate = self
+            $0.font = UIFont.systemFont(ofSize: 16*1.2, weight: .regular)
+            $0.textColor = .white
+            $0.sizeToFit()
+            $0.textContainer.lineFragmentPadding = 0
+            $0.textContainerInset = .zero
+        }
     }
     
     func layout() {
         view.addSubview(profileImage)
+        view.addSubview(nameModify)
+        view.addSubview(descripModify)
         profileImage.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
@@ -41,6 +57,39 @@ class ProfileModifyView: UIViewController {
             $0.widthAnchor.constraint(equalToConstant: Terminal.convertHeigt(value: 100)).isActive = true
             $0.heightAnchor.constraint(equalToConstant: Terminal.convertHeigt(value: 100)).isActive = true
         }
+        nameModify.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 20).isActive = true
+            $0.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        }
+        descripModify.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: nameModify.bottomAnchor, constant: 7).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
+            $0.heightAnchor.constraint(lessThanOrEqualToConstant: 10).isActive = true
+//            $0.heightAnchor.constraint(equalToConstant: 100).isActive = true
+//            $0.heightAnchor.constraint(equalTo: descripModify.heightAnchor).isActive = true
+        }
     }
 
+}
+
+
+extension ProfileModifyView: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        let size = CGSize(width: view.frame.width, height: .infinity)
+                let estimatedSize = textView.sizeThatFits(size)
+                textView.constraints.forEach { (constraint) in
+                    if constraint.firstAttribute == .height {
+                        constraint.constant = estimatedSize.height
+                    }
+                }
+//        let fixedWidth = textView.frame.size.width
+//        textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+//        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+//        var newFrame = textView.frame
+//        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+//        textView.frame = newFrame
+    }
 }
