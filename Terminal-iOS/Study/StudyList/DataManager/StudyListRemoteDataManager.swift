8506121595168aa8984b486377189402ec93fc6e
s-doris <swdoriz@gmail.com>
2020-11-12 01:59:59 +0900

    feat : study list api 구성

--- Terminal-iOS/Study/StudyList/DataManager/StudyListRemoteDataManager.swift
+++ Terminal-iOS/Study/StudyList/DataManager/StudyListRemoteDataManager.swift
@@ -7,11 +7,36 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class StudyListRemoteDataManager: StudyListRemoteDataManagerInputProtocol {
     var remoteRequestHandler: StudyListRemoteDataManagerOutputProtocol?
-    
-    func retrieveStudyList() {
+    var studyArr: [Study] = []
+    func retrieveStudyList(completionHandler: @escaping (([Study])) -> ()) {
+        let url = "http://3.35.154.27:3000/v1/study?category=IOS&sort=new"
+        AF.request(url).responseJSON { [self] response in
+            switch response.result {
+            case .success(let value):
+                if let data = JSON(value)["data"].array {
+                    do {
+                        for index in data {
+                            if index["title"].string != nil {
+                                print(index)
+                                let temp = try Study(title: index["title"].string!, subTitle: index["introduce"].string!, location: "강남구", date: index["created_at"].string!, managerImage: UIImage(named: "leehi")!, mainImage: UIImage(named: "mainImage")!)
+                                self.studyArr.append(temp)
+                            }
+                        }
+                        completionHandler(studyArr)
+                    } catch {
+                        print("error")
+                    }
+                    
+                }
+            case .failure(let err):
+                print(err)
+            }
+        }
         
     }
 }
