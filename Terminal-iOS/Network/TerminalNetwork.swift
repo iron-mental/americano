4405e9efe8b612409f62429ab3daab7138fd5c2e
s-doris <swdoriz@gmail.com>
2020-11-12 16:18:46 +0900

    style : 레거시 제거

--- Terminal-iOS/Network/TerminalNetwork.swift
+++ Terminal-iOS/Network/TerminalNetwork.swift
@@ -13,7 +13,7 @@ import SwiftyJSON
 class TerminalNetwork {
     static func getNewStudyList(_ category: String, _ sort: String, completionHandler: @escaping ([Study])->()) {
         let url = "http://3.35.154.27:3000/v1/study?category=\(category)&sort=\(sort)"
-
+        
         var studyArr: [Study] = []
         AF.request(url).responseJSON { response in
             switch response.result {
@@ -24,9 +24,9 @@ class TerminalNetwork {
                             if index["title"].string != nil {
                                 let temp = try! Study(title: index["title"].string!, subTitle: index["introduce"].string!, location: "강남구", date: index["created_at"].string!, managerImage: UIImage(named: "leehi")!, mainImage: UIImage(named: "mainImage")!)
                                 studyArr.append(temp)
-                                completionHandler(studyArr)
                             }
                         }
+                        completionHandler(studyArr)
                     } catch {
                         print("error")
                     }
@@ -38,23 +38,17 @@ class TerminalNetwork {
     }
     
     static func getNewStudyListForKey(_ keyValue: String, completionHandler: @escaping ([Study]) -> ()) {
-        let query = "3.35.154.27:3000/v1/study/paging/list?values=\(keyValue)"
+        let query = "http://3.35.154.27:3000/v1/study/paging/list?values=\(keyValue)"
         var studyArr: [Study] = []
         AF.request(query).responseJSON { response in
             switch response.result {
             case .success(let value):
                 if let data = JSON(value)["data"].array {
-//                    do {
-                        for index in data {
-//                            if index["title"].string != nil {
-                                let temp = Study(title: index["title"].string!, subTitle: index["introduce"].string!, location: "ㄹㅇㄹㅇ", date: index["created_at"].string!, managerImage: UIImage(named: "leehi")!, mainImage: UIImage(named: "mainImage")!)
-                                studyArr.append(temp)
-                                completionHandler(studyArr)
-//                            }
-                        }
-//                    } catch {
-//                        print("error")
-//                    }
+                    for index in data {
+                        let temp = Study(title: index["title"].string!, subTitle: index["introduce"].string!, location: "강남구", date: index["created_at"].string!, managerImage: UIImage(named: "leehi")!, mainImage: UIImage(named: "mainImage")!)
+                        studyArr.append(temp)
+                    }
+                    completionHandler(studyArr)
                 }
             case .failure(let err):
                 print(err)
