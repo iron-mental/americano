fb21695f7209d821a87df5c8436b42da4f914e2e
jeongjaein <wodls96@gmail.com>
2020-11-16 18:25:22 +0900

    feat : api 테스트를 위한  임시 토큰 추가

--- Terminal-iOS/Entity/Terminal.swift
+++ Terminal-iOS/Entity/Terminal.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class Terminal {
     
-    //kakao
-    //bce7f6969838f58c8c27fa25722c220b
+    
+    static let kakaoKey = "bce7f6969838f58c8c27fa25722c220b"
+    
     static let screenSize = UIScreen.main.bounds
+    
+    static let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOjEyLCJlbWFpbCI6Im1vY2tAbmF2ZXIuY29tIiwiaWF0IjoxNjA1NTE3ODMxLCJleHAiOjE2MDY4MTM4MzEsImlzcyI6InRlcm1pbmFsLXNlcnZlciIsInN1YiI6InVzZXJJbmZvLWFjY2VzcyJ9.CXSLLq_8iIarsskid4-6WZPXG6C_Q_CAR4ieVeukMys"
+    
+    static let refreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOjEyLCJpYXQiOjE2MDU1MTc4MzEsImV4cCI6MTYwNjgxMzgzMSwiaXNzIjoidGVybWluYWwtc2VydmVyIiwic3ViIjoidXNlckluZm8tcmVmcmVzaCJ9.mW08UmE1qr9WC3PmPXHnXvEmD_438b4_BwlJA_8e7yE"
 
     static func convertHeigt(value: CGFloat) -> CGFloat{
         return screenSize.height * (value / 667)
