4600741a7db15dfdb27a7de7518b07945691dcae
jeongjaein <wodls96@gmail.com>
2020-11-16 20:47:18 +0900

    feat : 임시 access token, refresh token 추가 후에 자기걸로 써야함

--- Terminal-iOS/Entity/Terminal.swift
+++ Terminal-iOS/Entity/Terminal.swift
@@ -10,12 +10,11 @@ import UIKit
 
 class Terminal {
     
-    
     static let kakaoKey = "bce7f6969838f58c8c27fa25722c220b"
     
     static let screenSize = UIScreen.main.bounds
     
-    static let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOjEyLCJlbWFpbCI6Im1vY2tAbmF2ZXIuY29tIiwiaWF0IjoxNjA1NTE3ODMxLCJleHAiOjE2MDY4MTM4MzEsImlzcyI6InRlcm1pbmFsLXNlcnZlciIsInN1YiI6InVzZXJJbmZvLWFjY2VzcyJ9.CXSLLq_8iIarsskid4-6WZPXG6C_Q_CAR4ieVeukMys"
+    static let accessToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOjEyLCJlbWFpbCI6Im1vY2tAbmF2ZXIuY29tIiwiaWF0IjoxNjA1NTE3ODMxLCJleHAiOjE2MDY4MTM4MzEsImlzcyI6InRlcm1pbmFsLXNlcnZlciIsInN1YiI6InVzZXJJbmZvLWFjY2VzcyJ9.CXSLLq_8iIarsskid4-6WZPXG6C_Q_CAR4ieVeukMys"
     
     static let refreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOjEyLCJpYXQiOjE2MDU1MTc4MzEsImV4cCI6MTYwNjgxMzgzMSwiaXNzIjoidGVybWluYWwtc2VydmVyIiwic3ViIjoidXNlckluZm8tcmVmcmVzaCJ9.mW08UmE1qr9WC3PmPXHnXvEmD_438b4_BwlJA_8e7yE"
 
