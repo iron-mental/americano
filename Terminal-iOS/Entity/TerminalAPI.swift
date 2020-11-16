74c48a1fbe4c7e594cdaef5fa6b127ebf4f52d13
jeongjaein <wodls96@gmail.com>
2020-11-16 20:46:52 +0900

    feat : TerminalAPI - getMyStudyList func 추가

--- Terminal-iOS/Entity/TerminalAPI.swift
+++ Terminal-iOS/Entity/TerminalAPI.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+class TerminalAPI {
+    static func getMyStudyList(completion: @escaping (_:Bool, _: [MyStudy]?) -> ()) {
+        let headers: HTTPHeaders = [ "Authorization": Terminal.accessToken]
+        var tempUserID = 1
+        //        let parameters: [String: String] = [
+        //            "query": text
+        //        ]
+        AF.request("http://3.35.154.27:3000/v1/user/\(tempUserID)/study",
+                   method: .get,headers: headers).responseJSON(completionHandler: { [self] response in
+                    switch response.result {
+                    case .success(let value):
+                    let json = JSON(value)["data"]
+                        json.array?.forEach { data in
+                            if let id = data["id"].int, let title = data["title"].string, let sigungu = data["sigungu"].string, let image = data["image"].string {
+                                TempMyStudyList.list.append(MyStudy(id: id, title: title, sigungu: sigungu, image: image))
+                            }
+                        }
+                        completion(true, TempMyStudyList.list)
+                    case .failure(let err) :
+                        print(err)
+                        completion(false, nil)
+                    }
+                   })
+    }
+}
