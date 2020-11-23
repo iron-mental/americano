aff9cb56063e44a0297c90777dc9a1ba786d116a
jeongjaein <wodls96@gmail.com>
2020-11-23 16:14:07 +0900

    feat : create ChatViewProtocols

---
+++ Terminal-iOS/MyStudy/StudyMain/ViewComponent/ChatView/ChatViewProtocols.swift
@@ -0,0 +1,33 @@
+//
+//  ChatViewProtocols.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/11/23.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+protocol ChatViewProtocol: UIViewController {
+    
+}
+
+protocol ChatInteractorProtocol {
+    
+}
+
+protocol ChatPresenterProtocol {
+    
+}
+
+protocol ChatRemoteDataManagerProtocol {
+    
+}
+
+protocol ChatLocalDataManagerProtocol {
+    
+}
+
+protocol ChatWireFrameProtocol {
+    
+}
