b19cfae766c502ac9cd8672e35e0c63d402faa9a
jeongjaein <wodls96@gmail.com>
2020-11-23 19:49:22 +0900

    feat : 채팅 뷰 custom cell

---
+++ Terminal-iOS/MyStudy/StudyMain/ViewComponent/ChatView/Component/ChatTableViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  ChatTableViewCell.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/11/23.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+    static var id = "ChatTableViewCell"
+    var chatLabel = UILabel()
+    var textInput = UITextField()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        attribute()
+        layout()
+    }
+    
+    func attribute() {
+        chatLabel.do {
+            $0.textColor = .white
+            $0.font = UIFont.monospacedSystemFont(ofSize: chatLabel.font.pointSize, weight: UIFont.Weight.regular)
+        }
+        textInput.do {
+            $0.textColor = .white
+            $0.font = UIFont.monospacedSystemFont(ofSize: chatLabel.font.pointSize, weight: UIFont.Weight.regular)
+            $0.isHidden = true
+        }
+    }
+    
+    func layout() {
+        [chatLabel, textInput].forEach { addSubview($0) }
+        
+        chatLabel.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: topAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+            $0.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        }
+        textInput.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: topAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+            $0.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        }
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
