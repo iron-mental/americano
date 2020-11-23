19c9f7161bb577a337ff698a9b81197a43289e5e
jeongjaein <wodls96@gmail.com>
2020-11-23 16:17:57 +0900

    feat : add instance of chat

--- Terminal-iOS/MyStudy/StudyMain/ViewComponent/ChatView/ChatViewProtocols.swift
+++ Terminal-iOS/MyStudy/StudyMain/ViewComponent/ChatView/ChatViewProtocols.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 protocol ChatViewProtocol: UIViewController {
-    
+    var presenter: ChatPresenterProtocol? { get set }
 }
 
 protocol ChatInteractorProtocol {
-    
+    var presenter: ChatPresenterProtocol? { get set }
+    var remoteDataManager: ChatRemoteDataManagerProtocol? { get set }
+    var localDataManager: ChatLocalDataManagerProtocol? { get set }
 }
 
 protocol ChatPresenterProtocol {
-    
+    var view: ChatViewProtocol? { get set }
+    var wireFrame: ChatWireFrameProtocol? { get set }
+    var interactor: ChatInteractorProtocol? { get set }
 }
 
 protocol ChatRemoteDataManagerProtocol {
@@ -29,5 +33,5 @@ protocol ChatLocalDataManagerProtocol {
 }
 
 protocol ChatWireFrameProtocol {
-    
+    var presenter: ChatPresenterProtocol? { get set }
 }
