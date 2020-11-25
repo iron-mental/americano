9ff333276e5f3254d76b4eef6e3d91ce96541db1
jeongjaein <wodls96@gmail.com>
2020-11-25 15:14:10 +0900

    머지 확인

--- Terminal-iOS/AppDelegate.swift
+++ Terminal-iOS/AppDelegate.swift
@@ -43,15 +43,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         
 //        MannaAPI.registerPushToken(pushToken: deviceTokenString)
     }
-
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let deviceTokenString = deviceToken.map { String(format: "%02x", $0) }.joined()
-                print("device 토큰 : ", deviceTokenString)
-        //        MannaAPI.registerPushToken(pushToken: deviceTokenString)
-    }
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert, .sound])
-    }
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
