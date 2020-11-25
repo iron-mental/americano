1519204a3d40f9b974b3511f76d24b21c11b7903
jeongjaein <wodls96@gmail.com>
2020-11-25 16:12:36 +0900

    feat : Notice VIPER 구성

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/Notice/NoticeProtocols.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/Notice/NoticeProtocols.swift
@@ -6,18 +6,22 @@
 //  Copyright © 2020 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 protocol NoticeViewProtocol {
-    
+    var presenter: NoticePresenterProtocol? { get set }
 }
 
 protocol NoticeInteractorProtocol {
-    
+    var presenter: NoticePresenterProtocol? { get set }
+    var remoteDataManager: NoticeRemoteDataManagerProtocol? { get set }
+    var localDataManager: NoticeLocalDataManagerProtocol? { get set }
 }
 
 protocol NoticePresenterProtocol {
-    
+    var view: NoticeViewProtocol? { get set }
+    var wireFrame: NoticeWireFrameProtocol? { get set }
+    var interactor: NoticeInteractorProtocol? { get set }
 }
 
 protocol NoticeRemoteDataManagerProtocol {
@@ -29,5 +33,7 @@ protocol NoticeLocalDataManagerProtocol {
 }
 
 protocol NoticeWireFrameProtocol {
+    var presenter: NoticePresenterProtocol? { get set }
     
+    static func createNoticeModule(studyID: Int) -> UIViewController
 }
