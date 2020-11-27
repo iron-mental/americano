1854fb109fc7e005ba3f648e993cdd78e8e49056
jeongjaein <wodls96@gmail.com>
2020-11-27 12:28:12 +0900

    feat : 스터디 생성 result 받아옴

--- Terminal-iOS/Study/CreateStudy/CreateStudyRemoteManager.swift
+++ Terminal-iOS/Study/CreateStudy/CreateStudyRemoteManager.swift
@@ -8,35 +8,10 @@
 
 import UIKit
 import Alamofire
-
-
-
-//user_id: 방장 id (require)
-//category: 분류 (require)
-//title: 길이 2~10 (require)
-//introduce: 길이 ~200 (require)
-//progress: 길이 ~100 (require)
-//study_time: 길이 ~100 (require)
-//latitude: 숫자 (require)
-//longitude: 숫자 (require)
-//region_1depth_name: 길이 ~20 (require)
-//region_2depth_name: 길이 ~20 (require)
-//address_name: 길이 ~100 (require)
-//locaion_detail: 길이 ~30
-//place_name: 길이 ~30
-//sns_notion: 길이 ~150
-//sns_evernote: 길이 ~60
-//sns_web: 길이 ~200
-//image: jpg, jpeg, png 외 불가
-
+import SwiftyJSON
 
 class CreateStudyRemoteManager: CreateStudyRemoteDataManagerProtocols {
-    
-    
-    
     func postStudy(study: StudyDetailPost) -> Bool {
-        print(study.location.detailAddress)
-        print(study.location.detailAddress!)
         let params : [String : Any] = [
             "category" : study.category,
             "title" : study.title,
@@ -55,7 +30,6 @@ class CreateStudyRemoteManager: CreateStudyRemoteDataManagerProtocols {
             "sns_web" : study.snsWeb! ?? "",
             "image" : study.image!
         ]
-        print(params)
         
         let header: HTTPHeaders = [ "Content-Type": "multipart/form-data",
                                     "Authorization": Terminal.accessToken]
@@ -67,8 +41,16 @@ class CreateStudyRemoteManager: CreateStudyRemoteDataManagerProtocols {
                 multipartFormData.append("\(value)".data(using: .utf8)!, withName: key, mimeType: "text/plain")
             }
             multipartFormData.append(imageData!, withName: "image", fileName: "\(study.category).jpg", mimeType: "image/jpeg")
-        }, to: "http://3.35.154.27:3000/v1/study", method: .post, headers: header).responseJSON { (test) in
-            print(test)
+        }, to: "http://3.35.154.27:3000/v1/study", method: .post, headers: header).responseJSON { (response) in
+            
+            switch response.result {
+            case .success(let value):
+                print(JSON(value)["message"].string!)
+                break
+            case .failure(let err):
+                print(err)
+                break
+            }
         }
         return true
         
