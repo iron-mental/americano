08532dec550650970dadbed978fd2ddc23f3b679
s-doris <swdoriz@gmail.com>
2020-11-27 15:23:09 +0900

    refactor : custom project view init param 추가

--- Terminal-iOS/Set/MyProfile/Component/ProjectView.swift
+++ Terminal-iOS/Set/MyProfile/Component/ProjectView.swift
@@ -9,57 +9,38 @@
 import UIKit
 
 class ProjectView: UIView {
-    let projectLabel = UILabel()
-    let projectTitle = UILabel()
-    let projectDescript = UILabel()
     
-    override init(frame: CGRect) {
+    init(title: String, contents: String ,frame: CGRect) {
         super.init(frame: frame)
-        attibute()
-        layout()
-    }
-    
-    func attibute() {
-        projectLabel.do {
-            $0.text = "프로젝트"
-            $0.textColor = .white
-        }
-        projectTitle.do {
-            $0.text = "Terminal"
+        let projectTitle = UILabel().then {
+            $0.text = title
             $0.textColor = .white
             $0.dynamicFont(fontSize: 20, weight: .bold)
         }
-        projectDescript.do {
-            $0.text = "MANNA는 어떠 어떠한 프로젝트이며 이러 이러 합니다. 저러 저러한 사람들이 쉽게 이러 이러하고 요로요로 어쩌고 저쩌고 하여 만들어진 프로젝트입니다. 이러이러한 걸 맡았고 어쩌고 저쩌고 하였습니다. 아래 github 링크에서 자세한 내용 확인하실 수 있습니다."
+        let projectContents = UILabel().then {
+            $0.text = contents
             $0.numberOfLines = 0
             $0.textColor = UIColor.appColor(.profileTextColor)
             $0.dynamicFont(fontSize: 16, weight: .regular)
         }
-    }
-    
-    func layout() {
-        addSubview(projectLabel)
-        addSubview(projectTitle)
-        addSubview(projectDescript)
         
-        projectLabel.do {
+        addSubview(projectTitle)
+        addSubview(projectContents)
+        
+        projectTitle.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: self.topAnchor, constant: 4).isActive = true
             $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
         }
-        projectTitle.do {
+        projectContents.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: self.projectLabel.bottomAnchor, constant: 4).isActive = true
-            $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
-        }
-        projectDescript.do {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: self.projectTitle.bottomAnchor, constant: 4).isActive = true
+            $0.topAnchor.constraint(equalTo: projectTitle.bottomAnchor, constant: 4).isActive = true
             $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
             $0.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
             $0.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
         }
     }
+   
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
