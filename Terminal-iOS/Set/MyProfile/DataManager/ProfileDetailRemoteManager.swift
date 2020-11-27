a08d9259f775258180adaca665a156175ddbb37a
s-doris <swdoriz@gmail.com>
2020-11-27 14:55:47 +0900

    feat : project api init

--- Terminal-iOS/Set/MyProfile/DataManager/ProfileDetailRemoteManager.swift
+++ Terminal-iOS/Set/MyProfile/DataManager/ProfileDetailRemoteManager.swift
@@ -13,6 +13,9 @@ import SwiftyJSON
 class ProfileDetailRemoteManager: ProfileDetailRemoteDataManagerInputProtocol {
     var remoteRequestHandler: ProfileDetailRemoteDataManagerOutputProtocol?
     
+    
+    // MARK: 유저정보
+    
     func getUserInfo(id: Int) {
         let url = "http://3.35.154.27:3000/v1/user/1"
         AF.request(url, headers: TerminalNetwork.headers).responseJSON { response in
@@ -22,6 +25,25 @@ class ProfileDetailRemoteManager: ProfileDetailRemoteDataManagerInputProtocol {
                 let data = "\(json)".data(using: .utf8)
                 let result = try! JSONDecoder().decode(BaseResponse<UserInfo>.self, from: data!)
                 self.remoteRequestHandler?.onUserInfoRetrieved(userInfo: result)
+            case .failure(let err):
+                print("실패")
+                print(err)
+            }
+        }
+    }
+    
+    // MARK: 유저 프로젝트
+    
+    func getProjectList(id: Int) {
+        let url = "http://3.35.154.27:3000/v1/user/1/project"
+        AF.request(url, headers: TerminalNetwork.headers).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let data = "\(json)".data(using: .utf8)
+                let result = try! JSONDecoder().decode(BaseResponse<[Project]>.self, from: data!)
+                print(result)
+//                self.remoteRequestHandler?.onUserInfoRetrieved(userInfo: result)
             case .failure(let err):
                 print("실패")
                 print(err)
