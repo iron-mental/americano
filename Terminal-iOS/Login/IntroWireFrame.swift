9e48d015d805b53aefa99ebb114e3fd4e01a9f26
s-doris <swdoriz@gmail.com>
2020-11-29 22:53:59 +0900

    feat : intro wireFrame init

---
+++ Terminal-iOS/Login/IntroWireFrame.swift
@@ -0,0 +1,36 @@
+//
+//  IntroWireFrame.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/11/29.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class IntroWireFrame: IntroWireFrameProtocol {
+    static func createIntroModule(beginState: BeginState, introState: IntroViewState) -> UIViewController {
+        
+        let view: IntroViewProtocol = IntroView()
+        let presenter: IntroPresenterProtocol = IntroPresenter()
+        let interactor: IntroInteractorProtocol = IntroInteractor()
+        let remoteDataManager: IntroRemoteDataManagerProtocol = IntroRemoteDataManager()
+        let localDataManager: IntroLocalDataManagerProtocol = IntroLocalDataManager()
+        let wireFrame: IntroWireFrameProtocol = IntroWireFrame()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.wireFrame = wireFrame
+        interactor.presenter = presenter
+        interactor.remoteDataManager = remoteDataManager
+        
+        if let view = view as? IntroView {
+            view.beginState = beginState
+            view.introState = introState
+            return view
+        } else {
+            return UIViewController()
+        }
+    }
+}
