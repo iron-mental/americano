e1583cee12d24451968477839da593f12809c1fa
s-doris <swdoriz@gmail.com>
2020-11-30 04:59:52 +0900

    feat : profileModify remoteManager init

--- Terminal-iOS/Set/ProfileModify/ProfileModifyRemoteManager.swift
+++ Terminal-iOS/Set/ProfileModify/ProfileModifyRemoteManager.swift
@@ -7,3 +7,9 @@
 //
 
 import Foundation
+
+class ProfileModifyRemoteManager: ProfileModifyRemoteDataManagerInputProtocol {
+    var remoteRequestHandler: ProfileModifyRemoteDataManagerOutputProtocol?
+    
+    
+}
