44498029a3362e0ff14b958a2ea2501a6b93ef89
s-doris <swdoriz@gmail.com>
2020-11-30 21:47:19 +0900

    feat : 프로필 수정용 모델 추가

---
+++ Terminal-iOS/Entity/UserInfoPut.swift
@@ -0,0 +1,26 @@
+//
+//  UserInfoPut.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/11/30.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+struct UserInfoPut {
+    let image: UIImage?
+    let nickname: String?
+    let introduce: String?
+    let careerTitle: String?
+    let careerContents: String?
+    let snsGithub: String?
+    let snsLinkedin: String?
+    let snsWeb: String?
+    let latitude: Double?
+    let longitude: Double?
+    let sido: String?
+    let sigungu: String?
+}
+
+
