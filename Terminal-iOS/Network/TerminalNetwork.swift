9ded37cf837b4c59c66a430d4407dc1247eb2da4
s-doris <swdoriz@gmail.com>
2020-12-02 21:29:01 +0900

    feat : login network init

--- Terminal-iOS/Network/TerminalNetwork.swift
+++ Terminal-iOS/Network/TerminalNetwork.swift
@@ -10,11 +10,34 @@ import UIKit
 import Alamofire
 import Kingfisher
 import SwiftyJSON
+import SwiftKeychainWrapper
 
 class TerminalNetwork {
     static let headers: HTTPHeaders = [
         "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJya2RjamYwMTIyQG5hdmVyLmNvbSIsIm5pY2tuYW1lIjoi64uJ64S067OA6rK97ZWo7JqUIiwiaWF0IjoxNjA2NDA1NDAxLCJleHAiOjEwNjA2NDA1NDAxLCJpc3MiOiJ0ZXJtaW5hbC1zZXJ2ZXIiLCJzdWIiOiJ1c2VySW5mby1hY2Nlc3MifQ.FgCJIyemTA0YGkVA2qlRhPgjvm3CrDH0enqX_u9JPmc"
     ]
+    
+    static func checkToekn(accessToken: String, completion: @escaping (BaseResponse<UserInfo>) -> Void) {
+        guard let userID = KeychainWrapper.standard.string(forKey: "userID") else { return }
+        let url = "http://3.35.154.27:3000/v1/user/\(userID)"
+        
+        let header: HTTPHeaders = [
+            "authorization": accessToken
+        ]
+        
+        AF.request(url, method: .get , headers: header).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                print(JSON(value))
+                let json = JSON(value)
+                let data = "\(json)".data(using: .utf8)
+                let result = try! JSONDecoder().decode(BaseResponse<UserInfo>.self, from: data!)
+                completion(result)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
     
     static func authRequest(refreshToken: String, accessToken: String, completion: @escaping (BaseResponse<Authorization>) -> Void) {
         let url = "http://3.35.154.27:3000/v1/user/reissuance"
@@ -29,6 +52,7 @@ class TerminalNetwork {
         AF.request(url, method: .post, parameters: parameters, headers: header).responseJSON { response in
             switch response.result {
             case .success(let value):
+                print(JSON(value))
                 let json = JSON(value)
                 let data = "\(json)".data(using: .utf8)
                 let result = try! JSONDecoder().decode(BaseResponse<Authorization>.self, from: data!)
