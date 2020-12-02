8bf555e2a7fa6cb9c8b1bb7e66ea324c71f5a01a
jeongjaein <wodls96@gmail.com>
2020-12-03 01:27:37 +0900

    feat : create LoadingRainbowCat

--- Terminal-iOS/Entity/LoadingRainbowCat.swift
+++ Terminal-iOS/Entity/LoadingRainbowCat.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2020 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Lottie
+
+class LoadingRainbowCat: NSObject {
+    private static let sharedInstance = LoadingRainbowCat()
+    //    var animationView = AnimationView(name:"12670-flying-airplane")
+    private var backgroundView: UIView?
+    private var popupView: AnimationView?
+    
+    class func show() {
+        let backgroundView = UIView(frame: CGRect.init(x: 0, y: 0, width: 100, height: 100))
+        let popupView = AnimationView(name:"14476-rainbow-cat-remix")
+        if let window = UIApplication.shared.keyWindow {
+            window.addSubview(backgroundView)
+            window.addSubview(popupView)
+            popupView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+            backgroundView.frame = CGRect(x: 0, y: 0, width: window.frame.maxX, height: window.frame.maxY)
+            backgroundView.backgroundColor = .black
+            backgroundView.alpha = 0.6
+            backgroundView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
+            
+            popupView.center = window.center
+            
+            sharedInstance.backgroundView?.removeFromSuperview()
+            sharedInstance.popupView?.removeFromSuperview()
+            sharedInstance.backgroundView = backgroundView
+            sharedInstance.popupView = popupView
+        }
+        
+        popupView.contentMode = .scaleAspectFit
+        popupView.play()
+        popupView.loopMode = .loop
+        
+    }
+    
+    class func hide(completion: @escaping () -> Void) {
+        if let popupView = sharedInstance.popupView,
+           let backgroundView = sharedInstance.backgroundView {
+            popupView.stop()
+            backgroundView.removeFromSuperview()
+            popupView.removeFromSuperview()
+        }
+        completion()
+    }
+}
