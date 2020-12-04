a51a36dc373171dac5a76f07d274f466351b64ca
s-doris <swdoriz@gmail.com>
2020-12-05 01:55:02 +0900

    feat : sns view component init

--- Terminal-iOS/Set/ProfileModify/Component/ProejctSNSView.swift
+++ Terminal-iOS/Set/ProfileModify/Component/ProejctSNSView.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class ProejctSNSView: SNSModifyView {
+    
+    override func attribute() {
+        super.attribute()
+        firstImage.do {
+            $0.image = UIImage(named: "github")
+        }
+        firstTextFeield.do {
+            $0.placeholder = "Github ID"
+        }
+        secondImage.do {
+            $0.image = UIImage(named: "app-store")
+        }
+        secondTextField.do {
+            $0.placeholder = "AppStore URL"
+        }
+        thirdImage.do {
+            $0.image = UIImage(named: "playstore")
+        }
+        thirdTextField.do {
+            $0.placeholder = "PlayStore URL"
+        }
+
+    }
+}
--- Terminal-iOS/Set/ProfileModify/Component/ProfileSNSView.swift
+++ Terminal-iOS/Set/ProfileModify/Component/ProfileSNSView.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2020 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class ProfileSNSView: SNSModifyView {
+    
+    override func attribute() {
+        super.attribute()
+        firstImage.do {
+            $0.image = UIImage(named: "github")
+        }
+        firstTextFeield.do {
+            $0.placeholder = "Github ID"
+        }
+        secondImage.do {
+            $0.image = UIImage(named: "linkedin")
+        }
+        secondTextField.do {
+            $0.placeholder = "LinkedIn URL"
+        }
+        thirdImage.do {
+            $0.image = UIImage(named: "web")
+        }
+        thirdTextField.do {
+            $0.placeholder = "URL"
+        }
+    }
+}
