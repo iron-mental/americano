317d3240a6458967d9b40ed690b6e61121361720
s-doris <swdoriz@gmail.com>
2020-12-09 23:51:43 +0900

    feat : Profile API 추가

--- Terminal-iOS/Set/MyProfile/DataManager/ProfileDetailRemoteManager.swift
+++ Terminal-iOS/Set/MyProfile/DataManager/ProfileDetailRemoteManager.swift
@@ -9,6 +9,7 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
+import SwiftKeychainWrapper
 
 class ProfileDetailRemoteManager: ProfileDetailRemoteDataManagerInputProtocol {
     var remoteRequestHandler: ProfileDetailRemoteDataManagerOutputProtocol?
@@ -16,26 +17,34 @@ class ProfileDetailRemoteManager: ProfileDetailRemoteDataManagerInputProtocol {
     // MARK: 유저정보
     
     func getUserInfo(id: Int) {
-        let url = "http://3.35.154.27:3000/v1/user/\(id)"
-        AF.request(url, headers: TerminalNetwork.headers).responseJSON { response in
-            switch response.result {
-            case .success(let value):
-                let json = JSON(value)
-                let data = "\(json)".data(using: .utf8)
-                let result = try! JSONDecoder().decode(BaseResponse<UserInfo>.self, from: data!)
-                self.remoteRequestHandler?.onUserInfoRetrieved(userInfo: result)
-            case .failure(let err):
-                print("실패")
-                print(err)
+        guard let userID = KeychainWrapper.standard.string(forKey: "userID") else { return }
+        TerminalNetworkManager
+            .shared
+            .session
+            .request(TerminalRouter.userInfo(id: userID))
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    let data = "\(json)".data(using: .utf8)
+                    let result = try! JSONDecoder().decode(BaseResponse<UserInfo>.self, from: data!)
+                    self.remoteRequestHandler?.onUserInfoRetrieved(userInfo: result)
+                case .failure(let err):
+                    print("실패")
+                    print(err)
+                }
             }
-        }
     }
     
     // MARK: 유저 프로젝트
     
     func getProjectList(id: Int) {
-        let url = "http://3.35.154.27:3000/v1/user/\(id)/project"
-        AF.request(url, headers: TerminalNetwork.headers).responseJSON { response in
+        guard let userID = KeychainWrapper.standard.string(forKey: "userID") else { return }
+        TerminalNetworkManager
+            .shared
+            .session
+            .request(TerminalRouter.projectList(id: userID))
+            .responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
