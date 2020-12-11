5ded6741f41b28de447182221853329bd64ab0d6
jeongjaein <wodls96@gmail.com>
2020-12-11 23:57:16 +0900

    feat : create LocalUserInfo for CoreData NSCoding

--- Terminal-iOS/CoreDataEntity/LocalUserInfo.swift
+++ Terminal-iOS/CoreDataEntity/LocalUserInfo.swift
@@ -1,1 +1,84 @@
+//
+//  LocalUserInfo.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/12/11.
+//  Copyright © 2020 정재인. All rights reserved.
+//
 
+import Foundation
+import CoreData
+
+class LocalUserInfo: NSObject, NSCoding {
+    let id: Int
+    let nickname, email: String
+    let image, introduce: String?
+    let address, careerTitle, careerContents: String?
+    let snsLinkedin, snsWeb, snsGithub: String?
+    let emailVerified: Bool
+    let createdAt: String
+    
+    enum Keys: String {
+        case id,
+             nickname,
+             email,
+             image,
+             introduce,
+             address,
+             careerTitle,
+             careerContents,
+             snsLinkedin,
+             snsWeb,
+             snsGithub,
+             emailVerified,
+             createdAt
+    }
+    init(userInfo: UserInfo) {
+        self.id = userInfo.id
+        self.nickname = userInfo.nickname
+        self.email = userInfo.email
+        self.image = userInfo.image
+        self.introduce = userInfo.introduce
+        self.address = userInfo.address
+        self.careerTitle = userInfo.careerTitle
+        self.careerContents = userInfo.careerContents
+        self.snsLinkedin = userInfo.snsLinkedin
+        self.snsWeb = userInfo.snsWeb
+        self.snsGithub = userInfo.snsGithub
+        self.emailVerified = userInfo.emailVerified
+        self.createdAt = userInfo.createdAt
+    }
+    
+    public func encode(with coder: NSCoder) {
+        coder.encode(id,forKey: Keys.id.rawValue)
+        coder.encode(nickname,forKey: Keys.nickname.rawValue)
+        coder.encode(email,forKey: Keys.email.rawValue)
+        coder.encode(image,forKey: Keys.image.rawValue)
+        coder.encode(introduce,forKey: Keys.introduce.rawValue)
+        coder.encode(address,forKey: Keys.address.rawValue)
+        coder.encode(careerTitle,forKey: Keys.careerTitle.rawValue)
+        coder.encode(careerContents,forKey: Keys.careerContents.rawValue)
+        coder.encode(snsLinkedin,forKey: Keys.snsLinkedin.rawValue)
+        coder.encode(snsWeb,forKey: Keys.snsWeb.rawValue)
+        coder.encode(snsGithub,forKey: Keys.snsGithub.rawValue)
+        coder.encode(emailVerified,forKey: Keys.emailVerified.rawValue)
+        coder.encode(createdAt,forKey: Keys.createdAt.rawValue)
+    }
+    
+    public required convenience init?(coder decoder: NSCoder) {
+        var decodedUserInfo = UserInfo(id: Int(decoder.decodeInt64(forKey: Keys.id.rawValue)),
+                 nickname: decoder.decodeObject(forKey: Keys.nickname.rawValue) as? String ?? "",
+                 email: decoder.decodeObject(forKey: Keys.email.rawValue) as? String ?? "",
+                 image: decoder.decodeObject(forKey: Keys.image.rawValue) as? String ?? "",
+                 introduce: decoder.decodeObject(forKey: Keys.introduce.rawValue) as? String ?? "",
+                 address: decoder.decodeObject(forKey: Keys.address.rawValue) as? String ?? "",
+                 careerTitle: decoder.decodeObject(forKey: Keys.careerTitle.rawValue) as? String ?? "",
+                 careerContents: decoder.decodeObject(forKey: Keys.careerContents.rawValue) as? String ?? "",
+                 snsLinkedin: decoder.decodeObject(forKey: Keys.snsLinkedin.rawValue) as? String ?? "",
+                 snsWeb: decoder.decodeObject(forKey: Keys.snsWeb.rawValue) as? String ?? "",
+                 snsGithub: decoder.decodeObject(forKey: Keys.snsGithub.rawValue) as? String ?? "",
+                 emailVerified: decoder.decodeBool(forKey: Keys.emailVerified.rawValue),
+                 createdAt: decoder.decodeObject(forKey: Keys.createdAt.rawValue) as? String ?? "")
+        self.init(userInfo: decodedUserInfo)
+    }
+}
