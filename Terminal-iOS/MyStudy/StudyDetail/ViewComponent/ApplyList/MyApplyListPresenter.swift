c60807704a2a998c3d083774b18d92bd93e5f388
s-doris <swdoriz@gmail.com>
2020-12-11 18:28:31 +0900

    feat : init apply presenter

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ApplyList/MyApplyListPresenter.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ApplyList/MyApplyListPresenter.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+ 
+class MyApplyListPresenter: MyApplyListPresenterProtocol {
+    var view: MyApplyListViewProtocol?
+    var interactor: MyApplyListInteractorInputProtocol?
+    var wireFrame: MyApplyListWireFrameProtocol?
+    
+    func viewDidLoad() {
+        interactor?.getApplyList()
+    }
+    
+    func showStudyDetail(keyValue: Int, state: Bool) {
+        
+    }
+}
+
+extension MyApplyListPresenter: MyApplyListInteractorOutputProtocol {
+    func didRetrieveStudies(studies: [Study]) {
+        
+    }
+    
+    func onError() {
+        
+    }
+    
+}
