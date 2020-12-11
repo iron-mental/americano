8b5c4ba0c8fa25379e7e7916018945e1d70c3ccf
s-doris <swdoriz@gmail.com>
2020-12-11 18:33:29 +0900

    feat : init apply view layout & attribute

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ApplyList/MyApplyListView.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ApplyList/MyApplyListView.swift
@@ -8,23 +8,59 @@
 
 import UIKit
 
-class MyApplyListView: UIViewController {
-
+final class MyApplyListView: UIViewController {
+    var presenter: MyApplyListPresenterProtocol?
+    var studyList: [Study] = []
+    lazy var applyList = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        presenter?.viewDidLoad()
+        attribute()
+        layout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func attribute() {
+        self.applyList.do {
+            $0.rowHeight = 80
+            $0.register(MyApplyListCell.self, forCellReuseIdentifier: MyApplyListCell.myApplyListCellID)
+            $0.delegate = self
+            $0.dataSource = self
+        }
     }
-    */
+    
+    private func layout() {
+        self.applyList.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+            $0.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        }
+    }
+}
 
+extension MyApplyListView: MyApplyListViewProtocol {
+    func showLoading() {
+        
+    }
+    
+    func hideLoading() {
+        
+    }
+}
+
+extension MyApplyListView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studyList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = applyList.dequeueReusableCell(withIdentifier: MyApplyListCell.myApplyListCellID,
+                                                 for: indexPath) as! MyApplyListCell
+        
+        return cell
+    }
+    
+    
 }
