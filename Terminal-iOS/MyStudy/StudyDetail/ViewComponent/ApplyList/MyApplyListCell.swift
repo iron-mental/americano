5a28f638148756664ebed319d9819557b7d2ea57
s-doris <swdoriz@gmail.com>
2020-12-11 18:27:07 +0900

    feat : init apply cell

---
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ApplyList/MyApplyListCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyApplyListCell.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/12/11.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import Foundation
+
+class MyApplyListCell: ApplyListCell {
+    static let myApplyListCellID = "MyApplyListCell"
+    
+}
