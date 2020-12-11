be1813b4f6ac4fef8ca8d530a2dfaf4acc3aa3db
s-doris <swdoriz@gmail.com>
2020-12-11 17:27:26 +0900

    feat : 베이스가 되는 신청목록 커스텀 셀 추가

---
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ApplyList/UserApplyView.swift
@@ -0,0 +1,30 @@
+//
+//  UserApplyView.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/12/11.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class UserApplyView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
