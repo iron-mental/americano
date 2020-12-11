0e6b91027dc586859a85c8d317b2fa31e3a9fe31
jeongjaein <wodls96@gmail.com>
2020-12-11 23:29:55 +0900

    feat : CoreDataManager 생성

--- Terminal-iOS/CoreDataEntity/LocalUserInfo.swift
+++ Terminal-iOS/CoreDataEntity/LocalUserInfo.swift
@@ -1,16 +1,9 @@
 //
-//  LocalStudyList+CoreDataClass.swift
+//  LocalUserInfo.swift
 //  Terminal-iOS
 //
 //  Created by 정재인 on 2020/12/11.
 //  Copyright © 2020 정재인. All rights reserved.
 //
-//
 
 import Foundation
-import CoreData
-
-@objc(LocalStudyList)
-public class LocalStudyList: NSManagedObject {
-
-}
--- Terminal-iOS/Entity/CoreDataManager.swift
+++ Terminal-iOS/Entity/CoreDataManager.swift
@@ -6,16 +6,12 @@
 //  Copyright © 2020 정재인. All rights reserved.
 //
 
-import CoreData
 import UIKit
-
+import CoreData
 
 class CoreDataManager {
-    static let shared: CoreDataManager = CoreDataManager()
-    let appDelegate: AppDelegate? = UIApplication.shared.delegate as? AppDelegate
-    lazy var context = appDelegate?.persistentContainer.viewContext
+    static let shared = CoreDataManager()
+    let appDelegate = (UIApplication.shared.delegate as! AppDelegate)
+    lazy var context = appDelegate.persistentContainer.viewContext
     
-    let modelName: String = ""
-    
-
 }
