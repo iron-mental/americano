5f422f6f972d2a9d25e8f5ae66d63fd8d644bd90
s-doris <swdoriz@gmail.com>
2020-12-11 23:55:11 +0900

    feat : apply user protocols init

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ApplyUser/ApplyUserProtocols.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ApplyUser/ApplyUserProtocols.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2020 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol ApplyUserViewProtocol: class {
+    var presenter: ApplyUserPresenterProtocol? { get set }
+    
+    // PRESENTER -> VIEW
+    func showStudyList(studies: [ApplyStudy]?)
+    func showLoading()
+    func hideLoading()
+}
+
+protocol ApplyUserWireFrameProtocol: class {
+    static func createStudyListModule() -> UIViewController
+    
+    // PRESENT -> WIREFRAME
+    func presentUserInfoDetailScreen(from view: ApplyUserViewProtocol)
+}
+
+protocol ApplyUserPresenterProtocol: class {
+    var view: ApplyUserViewProtocol? { get set }
+    var interactor: ApplyUserInteractorInputProtocol? { get set }
+    var wireFrame: ApplyUserWireFrameProtocol? { get set }
+    
+    // VIEW -> PRESENTER
+    func viewDidLoad()
+    func showStudyDetail(keyValue: Int, state: Bool)
+}
+
+protocol ApplyUserInteractorOutputProtocol: class {
+    // INTERACTOR -> PRESENTER
+    func didRetrieveStudies(studies: [ApplyStudy]?)
+    func onError()
+}
+
+protocol ApplyUserInteractorInputProtocol: class {
+    var presenter: MyApplyListInteractorOutputProtocol? { get set }
+    
+    // PRESENTER -> INTERACTOR
+    func getApplyList()
+}
