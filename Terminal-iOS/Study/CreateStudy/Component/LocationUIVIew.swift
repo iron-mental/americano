e93a1bc003cbc95ba42b251f46a46d86df6dbede
jeongjaein <wodls96@gmail.com>
2020-12-16 18:13:26 +0900

    refactor : LocationUIView detailAddress 추가

--- Terminal-iOS/Study/CreateStudy/Component/LocationUIVIew.swift
+++ Terminal-iOS/Study/CreateStudy/Component/LocationUIVIew.swift
@@ -11,9 +11,10 @@ import UIKit
 class LocationUIVIew: UIView {
     var title = UILabel()
     var detailAddress = UILabel()
+    var locationdetailTextField = UITextField()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init() {
+        super.init(frame: .zero)
         attribute()
         layout()
     }
@@ -21,30 +22,47 @@ class LocationUIVIew: UIView {
     func attribute() {
         title.do {
             $0.text = "장소"
+            $0.dynamicFont(fontSize: $0.font.pointSize , weight: .medium)
         }
         detailAddress.do {
-            $0.text = "상세 위치"
+            $0.sizeToFit()
             $0.backgroundColor = UIColor.appColor(.InputViewColor)
+            $0.layer.cornerRadius = 10
+            $0.layer.masksToBounds = true
+        }
+        locationdetailTextField.do {
+            $0.sizeToFit()
+            $0.backgroundColor = UIColor.appColor(.InputViewColor)
+            $0.layer.cornerRadius = 10
+            $0.layer.masksToBounds = true
+            $0.addLeftPadding()
         }
     }
     
     func layout() {
-        addSubview(title)
-        addSubview(detailAddress)
+        [title, detailAddress, locationdetailTextField].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
         
         title.do {
-            $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: topAnchor).isActive = true
             $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-            $0.widthAnchor.constraint(equalToConstant: (32/352) * frame.size.width).isActive = true
-            $0.heightAnchor.constraint(equalToConstant: (14/53) * frame.size.height).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: $0.intrinsicContentSize.height).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: $0.intrinsicContentSize.width).isActive = true
         }
         detailAddress.do {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: title.bottomAnchor, constant: (13/53) * frame.size.height).isActive = true
+            $0.topAnchor.constraint(equalTo: title.bottomAnchor,constant: Terminal.convertHeigt(value: 17)).isActive = true
             $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
             $0.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-            $0.heightAnchor.constraint(equalToConstant: (14/53) * frame.size.height).isActive = true
+            $0.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3).isActive = true
+        }
+        locationdetailTextField.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: detailAddress.bottomAnchor, constant: Terminal.convertHeigt(value: 17)).isActive = true
+            $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+            $0.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+            $0.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3).isActive = true
         }
     }
     required init?(coder: NSCoder) {
