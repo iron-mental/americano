83127f3196d04b45960cb0016262de4408ddb5b3
jeongjaein <wodls96@gmail.com>
2020-12-16 18:53:30 +0900

    refactor : TimeUIView autolayout 수정

--- Terminal-iOS/Study/CreateStudy/Component/TimeUIView.swift
+++ Terminal-iOS/Study/CreateStudy/Component/TimeUIView.swift
@@ -21,30 +21,35 @@ class TimeUIView: UIView {
     func attribute() {
         title.do {
             $0.text = "시간"
+            $0.dynamicFont(fontSize: $0.font.pointSize , weight: .medium)
         }
         detailTime.do {
+            $0.sizeToFit()
+            $0.layer.cornerRadius = 10
+            $0.layer.masksToBounds = true
             $0.placeholder = "시간정보 입력"
             $0.backgroundColor = UIColor.appColor(.InputViewColor)
+            $0.addLeftPadding()
         }
     }
     
     func layout() {
-        addSubview(title)
-        addSubview(detailTime)
+        [title, detailTime].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
         
         title.do {
-            $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: topAnchor).isActive = true
             $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-            $0.widthAnchor.constraint(equalToConstant: (32/352) * frame.size.width).isActive = true
-            $0.heightAnchor.constraint(equalToConstant: (14/53) * frame.size.height).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: $0.intrinsicContentSize.height).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: $0.intrinsicContentSize.width).isActive = true
         }
         detailTime.do {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: title.bottomAnchor, constant: (13/53) * frame.size.height).isActive = true
+            $0.topAnchor.constraint(equalTo: title.bottomAnchor, constant: Terminal.convertHeigt(value: 17)).isActive = true
             $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
             $0.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-            $0.heightAnchor.constraint(equalToConstant: (14/53) * frame.size.height).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: Terminal.convertHeigt(value: 45)).isActive = true
         }
     }
     required init?(coder: NSCoder) {
