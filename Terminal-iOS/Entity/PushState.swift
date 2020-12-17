048ae87375f0959d7c33c4fa2404f22434d490dd
s-doris <swdoriz@gmail.com>
2020-12-17 14:57:10 +0900

    feat : PushSatae enum class init

---
+++ Terminal-iOS/Entity/PushState.swift
@@ -0,0 +1,17 @@
+//
+//  PushState.swift
+//  Terminal-iOS
+//
+//  Created by once on 2020/12/17.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import Foundation
+
+enum PushState: String {
+    case applyNew = "apply_new"
+    case studyUpdate = "study_update"
+    case studyDelegate = "study_delegate"
+    case noticeNew = "notice_new"
+    case noticeUpdate = "notice_update"
+}
