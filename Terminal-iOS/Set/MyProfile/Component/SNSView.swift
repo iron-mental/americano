2ab6de718427556828cc50668a34c91ea8e4a59c
s-doris <swdoriz@gmail.com>
2020-12-21 18:48:02 +0900

    feat : snsView modify button init

--- Terminal-iOS/Set/MyProfile/Component/SNSView.swift
+++ Terminal-iOS/Set/MyProfile/Component/SNSView.swift
@@ -11,7 +11,8 @@ import UIKit
 class SNSView: UIView {
     let snsStack = UIStackView()
     let snsImage = UIImageView()
-        
+    let modify = UIButton()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         attribute()
@@ -22,18 +23,30 @@ class SNSView: UIView {
         snsImage.do {
             $0.image = #imageLiteral(resourceName: "github")
         }
+        modify.do {
+            $0.setTitle("수정", for: .normal)
+            $0.setTitleColor(.appColor(.mainColor), for: .normal)
+        }
     }
     
     func layout() {
-        addSubview(snsImage)
+        self.addSubview(self.snsImage)
+        self.addSubview(self.modify)
         
-        snsImage.do {
+        self.snsImage.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
             $0.widthAnchor.constraint(equalToConstant: 30).isActive = true
             $0.heightAnchor.constraint(equalToConstant: 30).isActive = true
             $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
             $0.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5).isActive = true
+        }
+        self.modify.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: self.topAnchor, constant: 5).isActive = true
+            $0.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 50).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 30).isActive = true
         }
     }
     
