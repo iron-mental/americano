9658c504f24e4c8ebf58fc24363672df2b606dc1
jeongjaein <wodls96@gmail.com>
2020-12-29 20:24:05 +0900

    feat : UILabel left margin simple function

--- Terminal-iOS/Extensions-Utillities/UILabel+Ext.swift
+++ Terminal-iOS/Extensions-Utillities/UILabel+Ext.swift
@@ -64,4 +64,16 @@ extension UILabel {
         
         self.attributedText = attributedString
     }
+    
+    func setMargins(margin: CGFloat = 20) {
+            if let textString = self.text {
+                var paragraphStyle = NSMutableParagraphStyle()
+                paragraphStyle.firstLineHeadIndent = margin
+                paragraphStyle.headIndent = margin
+                paragraphStyle.tailIndent = -margin
+                let attributedString = NSMutableAttributedString(string: textString)
+                attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+                attributedText = attributedString
+            }
+        }
 }
