cdba1fee9fc8c3bc4005b44286365e418dadecb3
jeongjaein <wodls96@gmail.com>
2020-12-30 21:26:52 +0900

    feat : SearchStudyProtocols 생성

---
+++ Terminal-iOS/Study/SearchStudy/SearchStudyProtocols.swift
@@ -0,0 +1,38 @@
+//
+//  SearchStudyProtocols.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/12/30.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+protocol SearchStudyViewProtocol {
+    var presenter: SearchStudyPresenterProtocol { get set }
+}
+
+protocol SearchStudyInteractorProtocol {
+    var presenter: SearchStudyPresenterProtocol { get set }
+    var remoteDataManager: SearchStudyRemoteDataManagerProtocol { get set }
+    var localDataManager: SearchStudyLocalDataManagerProtocol { get set }
+}
+
+protocol SearchStudyPresenterProtocol {
+    var view: SearchStudyViewProtocol { get set }
+    var interactor: SearchStudyInteractorProtocol { get set }
+}
+
+protocol SearchStudyRemoteDataManagerProtocol {
+    
+}
+
+protocol SearchStudyLocalDataManagerProtocol {
+    
+}
+
+protocol SearchStudyWireFrameProtocol {
+    var presenter: SearchStudyPresenterProtocol { get set }
+    
+    static func createSearchStudyModule() -> UIViewController
+}
