f64dbdedc948126ddc49bc3fd7aecd5fa84e7374
jeongjaein <wodls96@gmail.com>
2020-12-30 22:29:15 +0900

    feat : SearchStudyResultProtocols 작성

---
+++ Terminal-iOS/Study/SearchStudy/SearchStudyResultView/SearchStudyResultProtocols.swift
@@ -0,0 +1,38 @@
+//
+//  SearchStudyResultProtocols.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/12/30.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+protocol SearchStudyResultViewProtocol {
+    var presenter: SearchStudyResultPresenterProtocol? { get set }
+}
+
+protocol SearchStudyResultInteractorProtocol {
+    var presenter: SearchStudyResultPresenterProtocol? { get set }
+    var remoteDataManager: SearchStudyResultRemoteDataManagerProtocol? { get set }
+    var localDataManager: SearchStudyResultLocalDataManagerProtocol? { get set }
+}
+
+protocol SearchStudyResultPresenterProtocol {
+    var view: SearchStudyResultViewProtocol? { get set }
+    var interactor: SearchStudyResultInteractorProtocol? { get set }
+    var wireFrame: SearchStudyResultWireFrameProtocol? { get set }
+}
+
+protocol SearchStudyResultRemoteDataManagerProtocol {
+    
+}
+
+protocol SearchStudyResultLocalDataManagerProtocol {
+    
+}
+
+protocol SearchStudyResultWireFrameProtocol {
+    var presenter: SearchStudyResultPresenterProtocol? { get set }
+}
+
