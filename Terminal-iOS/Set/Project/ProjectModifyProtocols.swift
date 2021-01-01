08bcf1079d91ebc83d13c7bde18422c8d957fb7c
s-doris <swdoriz@gmail.com>
2021-01-02 02:15:01 +0900

    feat : projectModifyProtocols init

--- Terminal-iOS/Set/Project/ProjectModifyProtocols.swift
+++ Terminal-iOS/Set/Project/ProjectModifyProtocols.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2021 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol ProjectModifyViewProtocol: class {
+    var presenter: ProjectModifyPresenterProtocol? { get set }
+    
+    // PRESENTER -> VIEW
+    func modifyResultHandle(result: Bool, message: String)
+}
+
+protocol ProjectModifyWireFrameProtocol: class {
+    static func createModule() -> UIViewController
+}
+
+protocol ProjectModifyPresenterProtocol: class {
+    var view: ProjectModifyViewProtocol? { get set }
+    var interactor: ProjectModifyInteractorInputProtocol? { get set }
+    var wireFrame: ProjectModifyWireFrameProtocol? { get set }
+    
+    // VIEW -> PRESENTER
+    func completeModify(title: String, contents: String)
+}
+
+protocol ProjectModifyInteractorInputProtocol: class {
+    var presenter: ProjectModifyInteractorOutputProtocol? { get set }
+    
+    // PRESENTER -> INTERACTOR
+    func completeModify(title: String, contents: String)
+}
+
+protocol ProjectModifyInteractorOutputProtocol: class {
+    // INTERACTOR -> PRESENTER
+    func didCompleteModify(result: Bool, message: String)
+}
