917deb7e56d7b423cab1dd981e39c84811a0de60
s-doris <swdoriz@gmail.com>
2021-01-02 02:15:51 +0900

    feat : projectModifyWireFrame init

--- Terminal-iOS/Set/Project/ProjectModifyWireFrame.swift
+++ Terminal-iOS/Set/Project/ProjectModifyWireFrame.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2021 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ProjectModifyWireFrame: ProjectModifyWireFrameProtocol {
+    static func createModule() -> UIViewController {
+        let view: ProjectModifyViewProtocol = ProjectModifyView()
+        let presenter: ProjectModifyPresenterProtocol & ProjectModifyInteractorOutputProtocol = ProjectModifyPresenter()
+        let wireFrame: ProjectModifyWireFrameProtocol = ProjectModifyWireFrame()
+        let interactor: ProjectModifyInteractorInputProtocol = ProjectModifyInteractor()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.wireFrame = wireFrame
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+        
+        if let view = view as? ProjectModifyView {
+            return view
+        } else {
+            return UIViewController()
+        }
+    }
+    
+    
+}
