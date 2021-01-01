971c51da91c3ca645992fccc161d57930243136c
s-doris <swdoriz@gmail.com>
2021-01-02 02:05:08 +0900

    feat : projectModifyInteractor init

---
+++ Terminal-iOS/Set/Project/ProjectModifyInteractor.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectModifyInteractor.swift
+//  Terminal-iOS
+//
+//  Created by once on 2021/01/01.
+//  Copyright © 2021 정재인. All rights reserved.
+//
+
+import Foundation
+
+class ProjectModifyInteractor: ProjectModifyInteractorInputProtocol {
+    var presenter: ProjectModifyInteractorOutputProtocol?
+    
+    func completeModify(title: String, contents: String) {
+        
+    }
+}
---
+++ Terminal-iOS/Set/Project/ProjectModifyProtocols.swift
@@ -0,0 +1,9 @@
+//
+//  ProjectModifyProtocols.swift
+//  Terminal-iOS
+//
+//  Created by once on 2021/01/01.
+//  Copyright © 2021 정재인. All rights reserved.
+//
+
+import Foundation
