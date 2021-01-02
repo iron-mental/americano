9a0a26d3778ba7e2dfb32faf2e6f9006d3dedb62
jeongjaein <wodls96@gmail.com>
2021-01-02 21:47:06 +0900

    feat : SearchStudyResult 구조짜기시작

---
+++ Terminal-iOS/Study/SearchStudy/SearchStudyResultView/SearchStudyResultView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchStudyResultView.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2020/12/30.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class SearchStudyResultView: StudyListView {
+    var presenter: SearchStudyResultPresenterProtocol?
+}
+
+extension SearchStudyResultView: SearchStudyResultViewProtocol {
+    
+    
+}
