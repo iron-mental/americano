5e5a3f2eee0df3390cbb610e1c261490641b7bac
jeongjaein <wodls96@gmail.com>
2021-01-05 15:51:02 +0900

    refactor : create study를 재사용함 -> 레거시 삭제

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/StudyDetail/Component/TitleWithContentView.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/StudyDetail/Component/TitleWithContentView.swift
@@ -11,7 +11,6 @@ import UIKit
 class TitleWithContentView: UIView {
     var title = UILabel()
     var label = UILabel()
-    var textView = UITextView()
     var state: StudyDetailViewState = .none
     var contentText: [String] = ["기본 제목", "기본 텍스트"] {
         didSet {
@@ -19,15 +18,10 @@ class TitleWithContentView: UIView {
         }
     }
     
-    init(state: StudyDetailViewState) {
+    init() {
         super.init(frame: CGRect.zero)
         attribute()
-        self.state = state
-        if state == .none || state == .member{
-            labelLayout()
-        } else if state == .edit{
-            textViewLayout()
-        }
+        labelLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -55,23 +49,12 @@ class TitleWithContentView: UIView {
             $0.layer.cornerRadius = 10
             $0.layer.masksToBounds = true
         }
-        textView.do {
-            $0.font = UIFont.boldSystemFont(ofSize: 16)
-            $0.layer.cornerRadius = 10
-            $0.layer.masksToBounds = false
-            $0.text = contentText[1]
-            $0.backgroundColor = UIColor.appColor(.InputViewColor)
-            $0.sizeToFit()
-            $0.layer.cornerRadius = 10
-            $0.layer.masksToBounds = true
-        }
     }
     
     func labelLayout() {
-        [title, label, textView].forEach { addSubview($0) }
+        [title, label ].forEach { addSubview($0) }
         
         label.isHidden = false
-        textView.isHidden = true
         
         title.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
@@ -88,37 +71,10 @@ class TitleWithContentView: UIView {
             $0.heightAnchor.constraint(equalToConstant: label.intrinsicContentSize.height + 40).isActive = true
         }
     }
-    func textViewLayout() {
-        [title, label, textView].forEach { addSubview($0) }
-        
-        label.isHidden = true
-        textView.isHidden = false
-        
-        title.do {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: topAnchor).isActive = true
-            $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-            $0.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-            $0.heightAnchor.constraint(equalToConstant: title.intrinsicContentSize.height).isActive = true
-        }
-        textView.do {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: title.bottomAnchor, constant: Terminal.convertHeigt(value: 10)).isActive = true
-            $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-            $0.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-            $0.heightAnchor.constraint(equalToConstant: textView.intrinsicContentSize.height + 100).isActive = true
-        }
-    }
+    
     func titleHidden() {
         title.isHidden = true
         if self.state == .edit{
-            textView.do {
-                $0.translatesAutoresizingMaskIntoConstraints = false
-                $0.topAnchor.constraint(equalTo: title.bottomAnchor).isActive = true
-                $0.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-                $0.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-                $0.heightAnchor.constraint(equalTo: textView.heightAnchor).isActive = true
-            }
         } else {
             label.do {
                 $0.topAnchor.constraint(equalTo: topAnchor).isActive = true
