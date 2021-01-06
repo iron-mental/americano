83b165e52645f39dbfdb3a694bf2c91bf1d02833
s-doris <swdoriz@gmail.com>
2021-01-06 19:35:50 +0900

    feat : Profile modify API call 구성(Image, Info 각각)

--- Terminal-iOS/Set/Profile/ProfileModifyRemoteManager.swift
+++ Terminal-iOS/Set/Profile/ProfileModifyRemoteManager.swift
@@ -35,131 +35,51 @@ class ProfileModifyRemoteManager: ProfileModifyRemoteDataManagerInputProtocol {
             }
     }
     
-    func validProfileModify(userInfo: UserInfoPut) {
-        
-        let params: [String: String] = [
-            "nickname": userInfo.nickname!,
-            "introduce": userInfo.introduce!,
-            "career_title": userInfo.careerTitle!,
-            "career_contents": userInfo.careerContents!,
-            "sns_github": userInfo.snsGithub!,
-            "sns_linkedin": userInfo.snsLinkedIn!,
-            "sns_web": userInfo.snsWeb!,
-            "latitude": "\(userInfo.latitude!)",
-            "longitude": "\(userInfo.longitude!)",
-            "sido": userInfo.sido!,
-            "sigungu": userInfo.sigungu!
-        ]
-        
-        let uploadImage = userInfo.image!.jpegData(compressionQuality: 0.5)
+    func retrieveImageModify(image: UIImage) {
+        let uploadImage = image.jpegData(compressionQuality: 0.2)!
         guard let userID = KeychainWrapper.standard.string(forKey: "userID") else { return }
         
         TerminalNetworkManager
             .shared
             .session
             .upload(multipartFormData: { multipartFormData in
-                for (key, value) in params {
-                    let data = "\(value)".data(using: .utf8)!
-                    multipartFormData.append(data,
-                                             withName: key,
-                                             mimeType: "text/plain")
-                }
-                multipartFormData.append(uploadImage!,
+                multipartFormData.append(uploadImage,
                                          withName: "image",
-                                         fileName: "\(userInfo.nickname!).jpg",
+                                         fileName: "testImage.jpg",
                                          mimeType: "image/jpeg")
-            }, with: TerminalRouter.userInfoUpdate(id: userID))
+            }, with: TerminalRouter.userImageUpdate(id: userID))
             .validate(statusCode: 200..<299)
             .responseJSON { response in
                 switch response.result {
                 case .success(let value):
-                    print("여기닷:",JSON(value))
+                    let json = JSON(value)
+                    let data = "\(json)".data(using: .utf8)
+                    let result = try! JSONDecoder().decode(BaseResponse<Bool>.self, from: data!)
+                    self.remoteRequestHandler?.imageModifyRetrieved(result: result)
                 case .failure(let err):
                     print(err)
                 }
             }
     }
     
-    func remoteProjectList(completion: @escaping (BaseResponse<[Project]>) -> Void) {
-        guard let userID = KeychainWrapper.standard.string(forKey: "userID") else { return }
-
-        TerminalNetworkManager
-            .shared
-            .session
-            .request(TerminalRouter.projectList(id: userID))
-            .validate(statusCode: 200..<299)
-            .responseJSON { response in
-                switch response.result {
-                case .success(let value):
-                    print("성공:",JSON(value))
-                    let json = JSON(value)
-                    let data = "\(json)".data(using: .utf8)
-                    let result = try! JSONDecoder().decode(BaseResponse<[Project]>.self, from: data!)
-                    completion(result)
-                case .failure(let error):
-                    print("에러:", error)
-                }
-            }
-    }
-    
-    func removeProject(projectID: Int, completion: @escaping (Bool) -> Void) {
+    func retrieveNicknameModify(profile: [String: String]) {
+        let params: [String: String] = profile
         guard let userID = KeychainWrapper.standard.string(forKey: "userID") else { return }
         
         TerminalNetworkManager
             .shared
             .session
-            .request(TerminalRouter.projectDelete(id: userID, projectID: String(projectID)))
-            .validate(statusCode: 200..<299)
+            .request(TerminalRouter.userInfoUpdate(id: userID, profile: params))
+            .validate(statusCode: 200..<500)
             .responseJSON { response in
                 switch response.result {
                 case .success(let value):
                     let json = JSON(value)
                     let data = "\(json)".data(using: .utf8)
-                    do {
-                        let result = try JSONDecoder().decode(BaseResponse<Bool>.self, from: data!)
-                        if result.result {
-                            print("프로젝트 삭제완료")
-                            debugPrint(response)
-                            completion(true)
-                        } else {
-                            completion(false)
-                        }
-                    } catch {
-                        print("에러")
-                        completion(false)
-                    }
+                    let result = try! JSONDecoder().decode(BaseResponse<Bool>.self, from: data!)
+                    self.remoteRequestHandler?.nicknameModifyRetrieved(result: result)
                 case .failure(let error):
-                    print("에러:", error)
-                    completion(false)
-                }
-            }
-    }
-    
-    func registerProject(project: [String: String]) {
-        guard let userID = KeychainWrapper.standard.string(forKey: "userID") else { return }
-        TerminalNetworkManager
-            .shared
-            .session
-            .request(TerminalRouter.projectRegister(id: userID, project: project))
-            .validate(statusCode: 200..<299)
-            .responseJSON { response in
-                switch response.result {
-                case .success(let value):
-                    debugPrint(response)
-                    let json = JSON(value)
-                    let data = "\(json)".data(using: .utf8)
-                    do {
-                        let result = try JSONDecoder().decode(BaseResponse<Bool>.self, from: data!)
-                        if result.result {
-                            print("성공",result)
-                        } else {
-                            print("실패",result)
-                        }
-                    } catch {
-                        print("에러")
-                    }
-                case .failure(let error):
-                    print("에러:", error)
+                    print(error)
                 }
             }
     }
