0f1501c3b9720275b2dea0a7509944697da16dc9
s-doris <swdoriz@gmail.com>
2021-01-08 05:18:50 +0900

    feat : custom ProjectSNSView init

--- Terminal-iOS/Set/MyProfile/Component/ProjectSNSView.swift
+++ Terminal-iOS/Set/MyProfile/Component/ProjectSNSView.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2021 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ProjectSNSView: SNSView {
+    let github = UIButton().then {
+        $0.setImage(#imageLiteral(resourceName: "github"), for: .normal)
+    }
+    let appStore = UIButton().then {
+        $0.setImage(#imageLiteral(resourceName: "app-store"), for: .normal)
+    }
+    let playStore = UIButton().then {
+        $0.setImage(#imageLiteral(resourceName: "playstore"), for: .normal)
+    }
+    
+    override func addstack(snsList: [String : String]) {
+        /// 추가된 SNS 갯수
+        var count = 0
+        
+        if let _ = snsList["github"] {
+            self.snsStack.addArrangedSubview(self.github)
+                    count += 1
+                }
+        
+        if let _ = snsList["appStore"] {
+            self.snsStack.addArrangedSubview(self.appStore)
+            count += 1
+        }
+        
+        if let _ = snsList["playStore"] {
+            self.snsStack.addArrangedSubview(self.playStore)
+            count += 1
+        }
+        
+        switch count {
+        case 1:
+            self.firstWidth.isActive = true
+        case 2:
+            self.secondWidth.isActive = true
+        case 3:
+            self.thirdWidth.isActive = true
+        default:
+            break
+        }
+    
+    }
+}
