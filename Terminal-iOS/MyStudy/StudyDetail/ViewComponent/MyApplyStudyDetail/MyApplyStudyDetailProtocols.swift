f8307cf8e4a44e60684590c5ed60ba476d636945
jeongjaein <wodls96@gmail.com>
2021-01-11 18:13:21 +0900

    feat : create MyApplyStudyDetailProtocols

---
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/MyApplyStudyDetail/MyApplyStudyDetailProtocols.swift
@@ -0,0 +1,41 @@
+//
+//  MyApplyStudyDetailProtocols.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2021/01/11.
+//  Copyright © 2021 정재인. All rights reserved.
+//
+
+import UIKit
+
+protocol MyApplyStudyDetailViewProtocol {
+    
+}
+
+protocol MyApplyStudyDetailPresenterInputProtocol {
+    
+}
+
+protocol MyApplyStudyDetailPresenterOutputProtocol {
+    
+}
+
+protocol MyApplyStudyDetailInteractorInputProtocol {
+    
+}
+
+protocol MyApplyStudyDetailInteractorOutputProtocol {
+    
+}
+
+protocol MyApplyStudyDetailRemoteDataManagerInputProtocol {
+    
+}
+
+protocol MyApplyStudyDetailRemoteDataManagerOutputProtocol {
+    
+}
+
+protocol MyApplyStudyDetailWireFrameProtocol {
+    
+}
