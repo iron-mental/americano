2b0d8266e91e5c26c3d0ebb3c7f7885649fd7119
s-doris <swdoriz@gmail.com>
2021-01-12 21:21:18 +0900

    feat : LocationModifyInteractor init

--- Terminal-iOS/Set/Location/LocationModifyInteractor.swift
+++ Terminal-iOS/Set/Location/LocationModifyInteractor.swift
@@ -7,7 +7,29 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 class LocationModifyInteractor: LocationModifyInteractorInputProtocol {
     var presenter: LocationModifyInteractorOutputProtocol?
+    
+    func address() {
+        TerminalNetworkManager
+            .shared
+            .session
+            .request(TerminalRouter.address)
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    let data = "\(json)".data(using: .utf8)
+                    let result = try! JSONDecoder().decode(BaseResponse<[Address]>.self, from: data!)
+                    let isSuccess = result.result
+                    let addressList = result.data!
+                    self.presenter?.retrievedAddress(result: isSuccess, address: addressList)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
 }
