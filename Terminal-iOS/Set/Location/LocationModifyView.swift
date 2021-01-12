4d607d59bf27394eed968c281af981c8b7149a84
s-doris <swdoriz@gmail.com>
2021-01-12 23:14:31 +0900

    feat : 선택한 location -> UILabel에 반영 작업

--- Terminal-iOS/Set/Location/LocationModifyView.swift
+++ Terminal-iOS/Set/Location/LocationModifyView.swift
@@ -12,12 +12,30 @@ import Then
 class LocationModifyView: UIViewController {
     var presenter: LocationModifyPresenterProtocol?
     
-    var address1depth: [Address] = [] {
+    var addressState: AddressState = .si
+    var address1depth: [Address] = []
+    var address2depth: [String] = []
+    
+    var selectedLoaction: String = "" {
         didSet {
-            self.locationCollectionView.reloadData()
+            self.attribute()
+        }
+    }
+    var the1depth: String = "" {
+        didSet {
+            self.selectedLoaction
+                = self.the1depth + " " + self.the2depth
+        }
+    }
+    var the2depth: String = "" {
+        didSet {
+            self.selectedLoaction
+                = self.the1depth + " " + self.the2depth
         }
     }
     
+    lazy var locationLabel = UILabel()
+    lazy var completeButton = UIButton()
     let locationCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
@@ -32,53 +50,103 @@ class LocationModifyView: UIViewController {
     }
     
     private func attribute() {
-        locationCollectionView.do {
+        self.locationLabel.do {
+            $0.textColor = .white
+            $0.text = self.selectedLoaction
+        }
+        self.locationCollectionView.do {
             $0.backgroundColor = .systemBackground
             $0.register(LocationCell.self, forCellWithReuseIdentifier: LocationCell.cellID)
             $0.delegate = self
             $0.dataSource = self
         }
+        self.completeButton.do {
+            $0.backgroundColor = .appColor(.mainColor)
+            $0.setTitle("수정완료", for: .normal)
+            $0.setTitleColor(.white, for: .normal)
+            $0.layer.cornerRadius = 10
+            $0.addTarget(self, action: #selector(completeModify), for: .touchUpInside)
+        }
     }
     
     private func layout() {
+        self.view.addSubview(locationLabel)
         self.view.addSubview(locationCollectionView)
-        self.locationCollectionView.do {
+        self.view.addSubview(completeButton)
+        
+        self.locationLabel.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
             $0.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
-            $0.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
-            $0.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         }
+        
+        self.locationCollectionView.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: self.locationLabel.bottomAnchor, constant: 10).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
+            $0.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+            $0.bottomAnchor.constraint(equalTo: self.completeButton.topAnchor).isActive = true
+        }
+        
+        self.completeButton.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 10).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 25).isActive = true
+            $0.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -25).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        }
+    }
+    
+    @objc func completeModify() {
+        
     }
 }
 
 extension LocationModifyView: LocationModifyViewProtocol {
     func showAddress(address: [Address]) {
         self.address1depth = address
+        self.locationCollectionView.reloadData()
     }
 }
 
 extension LocationModifyView: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return address1depth.count
+        return addressState == .si ? address1depth.count : address2depth.count
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: UIScreen.main.bounds.width * 0.27,
-                              height: UIScreen.main.bounds.width * 0.13)
+                      height: UIScreen.main.bounds.width * 0.13)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = locationCollectionView.dequeueReusableCell(withReuseIdentifier: LocationCell.cellID, for: indexPath) as! LocationCell
         
-        let data = address1depth[indexPath.row].si
-        cell.setData(data: data)
+        switch addressState {
+        case .si:
+            let data = address1depth[indexPath.row].si
+            cell.setData(data: data)
+        case .gunGu:
+            let data = address2depth[indexPath.row]
+            cell.setData(data: data)
+        }
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(indexPath.row)
+        if addressState == .si {
+            let gungu = address1depth[indexPath.row].gunGu
+            self.address2depth = gungu
+            
+            self.the1depth = address1depth[indexPath.row].si
+            
+            self.addressState = .gunGu
+            self.locationCollectionView.reloadData()
+        } else {
+            let gungu = address2depth[indexPath.row]
+            self.the2depth = gungu
+        }
     }
 }
