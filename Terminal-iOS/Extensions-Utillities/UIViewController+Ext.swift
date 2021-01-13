24530a0040938a3bae3c799d164aa944fb5a390b
s-doris <swdoriz@gmail.com>
2021-01-13 17:59:54 +0900

    feat : toast message extension init

---
+++ Terminal-iOS/Extensions-Utillities/UIViewController+Ext.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewController+Ext.swift
+//  Terminal-iOS
+//
+//  Created by once on 2021/01/13.
+//  Copyright © 2021 정재인. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showToast(controller: UIViewController, message: String, seconds: Double) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        alert.view.backgroundColor = .appColor(.terminalBackground)
+        alert.view.alpha = 0.5
+        alert.view.layer.cornerRadius = 20
+        
+        let titleFont = [NSAttributedString.Key.font: UIFont(name: "NotoSansKR-Medium", size: 15)]
+        let titleAttrString = NSMutableAttributedString(string: message, attributes: titleFont)
+
+        alert.setValue(titleAttrString, forKey:"attributedTitle")
+        
+        
+        controller.present(alert, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            alert.dismiss(animated: true)
+        }
+    }
+}
