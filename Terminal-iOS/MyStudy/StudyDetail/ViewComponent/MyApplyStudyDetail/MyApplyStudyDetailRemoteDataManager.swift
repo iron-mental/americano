9e4e29d3c839cf366221cde912a5578720956498
jeongjaein <54730280+jeongjaein@users.noreply.github.com>
2021-01-15 20:24:26 +0900

    Update Terminal-iOS/MyStudy/StudyDetail/ViewComponent/MyApplyStudyDetail/MyApplyStudyDetailRemoteDataManager.swift
    Co-authored-by: Yoon Sangwon <59601439+feelsodev@users.noreply.github.com>

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/MyApplyStudyDetail/MyApplyStudyDetailRemoteDataManager.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/MyApplyStudyDetail/MyApplyStudyDetailRemoteDataManager.swift
@@ -22,7 +22,6 @@ class MyApplyStudyDetailRemoteDataManager: MyApplyStudyDetailRemoteDataManagerIn
             .responseJSON { response in
                 switch response.result {
                 case .success(let value):
-                    
                     let json = JSON(value)
                     let data = "\(json)".data(using: .utf8)
                     do {
