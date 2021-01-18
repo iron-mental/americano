729cad5aef92f410d80d2cc85442a05304c80712
s-doris <swdoriz@gmail.com>
2021-01-18 21:36:50 +0900

    feat : EmailAuth AlertView 적용

--- Terminal-iOS/Set/EmailAuth/EmailAuthView.swift
+++ Terminal-iOS/Set/EmailAuth/EmailAuthView.swift
@@ -11,11 +11,8 @@ import UIKit
 class EmailAuthView: UIViewController {
     var presenter: EmailAuthPresenterProtocol?
     
-    let content = UILabel()
-    let buttonStack = UIStackView()
-    let refuseButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 60))
-    let acceptButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 60))
-    
+    let emailAuth = EmailAlertMessageView(message: "이메일 인증하시겠습니까?\n\n 회원님의 이메일로 인증요청 됩니다.")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         attribute()
@@ -23,62 +20,36 @@ class EmailAuthView: UIViewController {
     }
     
     private func attribute() {
-        self.view.backgroundColor = .appColor(.terminalBackground)
-        self.content.do {
-            $0.text = "이메일을 인증 하시겠습니까?\n 회원님의 이메일은 swdoris@gamill.cim입니다.\n 이메일로 인증요청이 "
+        emailAuth.alertMessageLabel.do {
+            $0.textColor = UIColor.appColor(.alertTextcolor)
             $0.numberOfLines = 0
-            $0.textColor = .white
+            $0.textAlignment = .center
+            $0.font = UIFont.monospacedSystemFont(ofSize: $0.font.pointSize, weight: UIFont.Weight.regular)
         }
-        
-        self.buttonStack.do {
-            $0.axis = .horizontal
-            $0.spacing = 10
-            $0.distribution = .fillEqually
-            $0.addArrangedSubview(self.refuseButton)
-            $0.addArrangedSubview(self.acceptButton)
-        }
-        
-        self.refuseButton.do {
-            $0.backgroundColor = .red
-            $0.setTitle("취소", for: .normal)
-            $0.setTitleColor(.white, for: .normal)
-            $0.layer.cornerRadius = 10
-            $0.addTarget(self, action: #selector(refuse), for: .touchUpInside)
-        }
-        
-        self.acceptButton.do {
-            $0.backgroundColor = .appColor(.mainColor)
-            $0.setTitle("확인", for: .normal)
-            $0.setTitleColor(.white, for: .normal)
-            $0.layer.cornerRadius = 10
-            $0.addTarget(self, action: #selector(accept), for: .touchUpInside)
-        }
+            
+        let button = self.emailAuth as AlertBaseUIView
+        button.completeButton.addTarget(self, action: #selector(completeClick), for: .touchUpInside)
+        button.dismissButton.addTarget(self, action: #selector(dismissClick), for: .touchUpInside)
     }
     
     private func layout() {
-        self.view.addSubview(self.content)
-        self.view.addSubview(self.buttonStack)
+        self.view.addSubview(emailAuth)
         
-        self.content.do {
+        self.emailAuth.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 200).isActive = true
             $0.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        }
-        self.buttonStack.do {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: self.content.bottomAnchor, constant: 30).isActive = true
-            $0.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-            $0.heightAnchor.constraint(equalToConstant: 60).isActive = true
-            $0.widthAnchor.constraint(equalToConstant: 220).isActive = true
+            $0.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -20).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 300).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: self.view.frame.width * 0.8).isActive = true
         }
     }
     
-    @objc func accept() {
-        presenter?.emailAuthRequest()
-    }
-    
-    @objc func refuse() {
+    @objc func dismissClick() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func completeClick() {
+        presenter?.emailAuthRequest()
     }
 }
 
---
+++ Terminal-iOS/StaticViewComponent/EmailAlertMessageView.swift
@@ -0,0 +1,9 @@
+//
+//  EmailAlertMessageView.swift
+//  Terminal-iOS
+//
+//  Created by once on 2021/01/18.
+//  Copyright © 2021 정재인. All rights reserved.
+//
+
+import Foundation
