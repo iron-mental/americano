1b4465b90356e0e9bc178a37afae166d4823ac89
jeongjaein <wodls96@gmail.com>
2021-01-21 22:03:57 +0900

    feat : DelegateHostProtocols function 정의

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/DelegateHost/DelegateHostProtocols.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/DelegateHost/DelegateHostProtocols.swift
@@ -11,28 +11,45 @@ import UIKit
 protocol DelegateHostViewProtocol {
     var presenter: DelegateHostPresenterProtocol? { get set }
     var userList: [Participate]? { get set }
+    
+    //PRESENTER -> VIEW
+    func showDelegateHostResult(message: String)
+    func showError()
 }
 
 protocol DelegateHostPresenterProtocol {
     var view: DelegateHostViewProtocol? { get set }
     var interactor: DelegateHostInteractorInputProtocol? { get set }
     var wireFrame: DelegateHostWireFrameProtocol? { get set }
+    
+    //VIEW -> PRESENTER
+    func delegateHostButtonDidTap(newLeader: Int)
 }
 
 protocol DelegateHostInteractorInputProtocol {
     var presenter: DelegateHostInteractorOutputProtocol? { get set }
     var remoteDataManager: DelegateHostPresenterProtocol? { get set }
+    var studyID: Int? { get set }
+    
+    //PRESENTER -> INTERACTOR
+    func putDelegateHostAPI(newLeader: Int)
 }
 
 protocol DelegateHostInteractorOutputProtocol {
+    //INTERACTOR -> PRESENTER
+    func delegateHostResult(result: Bool, message: String)
 }
 
 protocol DelegateHostRemoteDataManagerInputProtocol {
     var interactor: DelegateHostInteractorOutputProtocol? { get set }
+    
+    //INTERACTOR -> REMOTEDATAMANAGER
+    func putDelegateHostAPI(studyID: Int, newLeader: Int)
 }
 
 protocol DelegateHostRemoteDataManagerOutputProtocol {
-    
+    //REMOTEDATAMANAGER -> INTERACTOR
+    func delegateHostResult(response: BaseResponse<String>)
 }
 
 protocol DelegateHostWireFrameProtocol {
