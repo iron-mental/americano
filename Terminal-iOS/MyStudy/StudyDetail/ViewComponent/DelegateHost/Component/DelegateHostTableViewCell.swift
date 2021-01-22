b8fb37a9f3026a5af27cecb86cb4710cb212a5bf
jeongjaein <wodls96@gmail.com>
2021-01-22 14:36:18 +0900

    feat : DelegateHostView 구성

---
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/DelegateHost/Component/DelegateHostTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  DelegateHostTableViewCell.swift
+//  Terminal-iOS
+//
+//  Created by 정재인 on 2021/01/22.
+//  Copyright © 2021 정재인. All rights reserved.
+//
+
+import Foundation
--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/DelegateHost/DelegateHostView.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/DelegateHost/DelegateHostView.swift
@@ -12,32 +12,70 @@ class DelegateHostView: UIViewController {
     var presenter: DelegateHostPresenterProtocol?
     var userList: [Participate]?
     var userTableView = UITableView()
+    var guideLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        attribute()
+        layout()
+        
+    }
+    
+    func attribute() {
+        view.do {
+            $0.backgroundColor = UIColor.appColor(.terminalBackground)
+        }
+        guideLabel.do {
+            $0.text = "방장을 위임할 멤버를 골라주세요"
+            $0.numberOfLines = 0
+            $0.font = UIFont.boldSystemFont(ofSize: 28)
+            
+        }
+        userTableView.do {
+            $0.delegate = self
+            $0.dataSource = self
+            $0.backgroundColor = UIColor.appColor(.terminalBackground)
+        }
+    }
+    
+    func layout() {
+        [ guideLabel, userTableView ].forEach { view.addSubview($0) }
+        
+        guideLabel.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        }
+        userTableView.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: guideLabel.bottomAnchor, constant: Terminal.convertHeigt(value: 10)).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            $0.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        }
     }
 }
 
 extension DelegateHostView: DelegateHostViewProtocol {
     func showDelegateHostResult(message: String) {
-//        <#code#>
+        //        <#code#>
     }
     
     func showError() {
-//        <#code#>
+        //        <#code#>
     }
 }
 
 extension DelegateHostView: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return userList!.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        return UITableViewCell()
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        //        <#code#>
     }
 }
