8555d9e9aa028b78d16845b63c19ed4d8e9bba5c
s-doris <swdoriz@gmail.com>
2021-01-26 02:15:23 +0900

    refactor : 레거시 제거

--- Terminal-iOS/Study/StudyList/StudyListView.swift
+++ Terminal-iOS/Study/StudyList/StudyListView.swift
@@ -11,14 +11,12 @@ import Then
 
 class StudyListView: UIViewController {
     var category: String?
-    var sort: String?
     let tableView = UITableView()
     let aligmentView = UIView()
     let lateButton = UIButton()
     let locationButton = UIButton()
     let selectedUnderline = UIView()
     let refreshControl = UIRefreshControl()
-    var enumValue: AssetsColor = .testColor
     var sortState: SortState = .new
     
     var presenter: StudyListPresenterProtocol?
