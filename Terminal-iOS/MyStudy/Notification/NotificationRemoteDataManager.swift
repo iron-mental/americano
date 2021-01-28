dce99622541d21e96a09bf89dad636b56eccd09d
s-doris <swdoriz@gmail.com>
2021-01-28 17:26:16 +0900

    feat : NotificationRemoteDataManager 추가에 따른 변경사항 반영

---
+++ Terminal-iOS/MyStudy/Notification/NotificationRemoteDataManager.swift
@@ -0,0 +1,40 @@
+//
+//  NotificationRemoteDataManager.swift
+//  Terminal-iOS
+//
+//  Created by once on 2021/01/28.
+//  Copyright © 2021 정재인. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import SwiftKeychainWrapper
+
+class NotificationRemoteDataManager: NotificationRemoteDataManagerInputProtocol {
+    var interactor: NotificationRemoteDataManagerOutputProtocol?
+    
+    func retrieveAlert() {
+        guard let userID = KeychainWrapper.standard.string(forKey: "userID") else { return }
+        
+        TerminalNetworkManager
+            .shared
+            .session
+            .request(TerminalRouter.alert(id: userID))
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    let data = "\(json)".data(using: .utf8)
+                    do {
+                        let result = try JSONDecoder().decode(BaseResponse<[Noti]>.self, from: data!)
+                        self.interactor?.onRetrievedAlert(result: result)
+                    } catch {
+                        print(error)
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            } 
+    }
+}
--- Terminal-iOS/MyStudy/Notification/NotificationWireFrame.swift
+++ Terminal-iOS/MyStudy/Notification/NotificationWireFrame.swift
@@ -11,15 +11,20 @@ import UIKit
 class NotificationWireFrame: NotificationWireFrameProtocol {
     static func createModule() -> UIViewController {
         let view: NotificationViewProtocol = NotificationView()
-        let presenter: NotificationPresenterProtocol & NotificationInteractorOutputProtocol = NotificationPresenter()
-        let interactor: NotificationInteractorInputProtocol = NotificationInteractor()
+        let presenter: NotificationPresenterProtocol
+            & NotificationInteractorOutputProtocol = NotificationPresenter()
+        let interactor: NotificationInteractorInputProtocol
+            & NotificationRemoteDataManagerOutputProtocol = NotificationInteractor()
         let wireFrame: NotificationWireFrameProtocol = NotificationWireFrame()
+        let remoteDataManager: NotificationRemoteDataManagerInputProtocol = NotificationRemoteDataManager()
         
         view.presenter = presenter
         presenter.view = view
         presenter.interactor = interactor
         presenter.wireFrame = wireFrame
         interactor.presenter = presenter
+        interactor.remoteDataManager = remoteDataManager
+        remoteDataManager.interactor = interactor
         
         if let view = view as? UIViewController {
             return view
