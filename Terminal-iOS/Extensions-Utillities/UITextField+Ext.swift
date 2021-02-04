fdb6f859649a33905ce937ae02ea20944b3aab52
jeongjaein <54730280+jeongjaein@users.noreply.github.com>
2021-02-05 00:14:31 +0900

    Update Terminal-iOS/Extensions-Utillities/UITextField+Ext.swift

--- Terminal-iOS/Extensions-Utillities/UITextField+Ext.swift
+++ Terminal-iOS/Extensions-Utillities/UITextField+Ext.swift
@@ -14,6 +14,7 @@ extension UITextField {
         self.leftView = paddingView
         self.leftViewMode = ViewMode.always
     }
+    
     func addRightPadding(padding: CGFloat) {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: padding, height: self.frame.height))
         self.rightView = paddingView
