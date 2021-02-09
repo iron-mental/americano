cf169ae8845e417c37df2deaae395f12a7402725
jeongjaein <wodls96@gmail.com>
2021-02-10 01:37:13 +0900

    feat : getSearchResultByLocalName func 추가

--- Terminal-iOS/Study/CreateStudy/LocationView/SearchLoaction/SearchLocationRemoteDataManager.swift
+++ Terminal-iOS/Study/CreateStudy/LocationView/SearchLoaction/SearchLocationRemoteDataManager.swift
@@ -13,31 +13,63 @@ import SwiftyJSON
 class SearchLocationRemoteDataManager: SearchLocationRemoteDataManagerProtocol {
     func getSearchResultByKeyword(text: String, completionHandler: @escaping (_: Bool, _ list: [StudyDetailLocationPost]) -> Void) {
         var resultList: [StudyDetailLocationPost] = []
-        var result = false
         let headers: HTTPHeaders = [ "Authorization": "KakaoAK 6cd40b04c090b1a033634e5051aab78c" ]
-        let parameters: [String: String] = [
-            "query": text
-        ]
+        let parameters: [String: String] = [ "query": text ]
+        
         AF.request("https://dapi.kakao.com/v2/local/search/keyword.json",
                    method: .get,
                    parameters: parameters, headers: headers).responseJSON(completionHandler: { response in
                     switch response.result {
                     case .success(let value):
                         if JSON(value)["documents"].count == 0 {
-                            result = false
+                            //여기다가 하면 될듯?
+                            let localSearchResult = self.getSearchResultByLocalName(text: text)
+                            completionHandler(localSearchResult.result, localSearchResult.item)
                         } else {
-                            
                             if let addressList = JSON(value)["documents"].array {
                                 for item in addressList {
                                     let address = item["road_address_name"].string!.isEmpty ? item["address_name"].string! : item["road_address_name"].string!
                                     let newItem = StudyDetailLocationPost(address: address,
-                                                                    lat: Double(item["y"].string!) ?? 0,
-                                                                    lng: Double(item["x"].string!) ?? 0,
-                                                                    placeName: item["place_name"].string ?? nil ,
-                                                                    category: item["category_group_name"].string!
-                                                                    )
+                                                                          lat: Double(item["y"].string!) ?? 0,
+                                                                          lng: Double(item["x"].string!) ?? 0,
+                                                                          placeName: item["place_name"].string ?? nil ,
+                                                                          category: item["category_group_name"].string!
+                                    )
                                     resultList.append(newItem)
+                                }
+                            }
+                            completionHandler(true, resultList)
+                        }
+                    case .failure(let err) :
+                        print(err)
+                    }
+                   })
+    }
+    
+    func getSearchResultByLocalName(text: String) -> (result: Bool, item: [StudyDetailLocationPost]) {
+        var resultList: [StudyDetailLocationPost] = []
+        let headers: HTTPHeaders = [ "Authorization": "KakaoAK 6cd40b04c090b1a033634e5051aab78c" ]
+        let parameters: [String: String] = [ "query": text ]
+        var result = false
+        AF.request("https://dapi.kakao.com/v2/local/search/address.json",
+                   method: .get,
+                   parameters: parameters, headers: headers).responseJSON(completionHandler: { response in
+                    switch response.result {
+                    case .success(let value):
+                        if JSON(value)["documents"].count == 0 {
+                            
+                        } else {
+                            if let addressList = JSON(value)["documents"].array {
+                                for item in addressList {
+                                    let address = item["road_address"]["address_name"].string!.isEmpty ? item["address_name"].string! : item["road_address"]["address_name"].string!
                                     
+                                    let newItem = StudyDetailLocationPost(address: address,
+                                                                          lat: Double(item["y"].string!) ?? 0,
+                                                                          lng: Double(item["x"].string!) ?? 0,
+                                                                          placeName: item["road_address"]["building_name"].string ?? "" ,
+                                                                          category: ""
+                                    )
+                                    resultList.append(newItem)
                                 }
                             }
                             result = true
@@ -45,8 +77,7 @@ class SearchLocationRemoteDataManager: SearchLocationRemoteDataManagerProtocol {
                     case .failure(let err) :
                         print(err)
                     }
-                    completionHandler(result, resultList)
                    })
+        return (result, resultList)
     }
-    
 }
