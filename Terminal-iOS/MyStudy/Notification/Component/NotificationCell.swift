9fc5551c04ad6444a30db82374e4e58c2e7fa621
jeongjaein <wodls96@gmail.com>
2021-02-16 18:04:02 +0900

    feat : notification convertToElapsedTime, split 구현

--- Terminal-iOS/MyStudy/Notification/Component/NotificationCell.swift
+++ Terminal-iOS/MyStudy/Notification/Component/NotificationCell.swift
@@ -16,6 +16,8 @@ class NotificationCell: UITableViewCell {
     let explainLabel = UILabel()
     let timeLabel = UILabel()
     
+    let now = Date()
+    let date = DateFormatter()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -71,7 +73,7 @@ class NotificationCell: UITableViewCell {
         self.timeLabel.do {
             $0.text = "3일 전"
             $0.textColor = .systemGray
-            $0.dynamicFont(fontSize: 14, weight: .bold)
+            $0.dynamicFont(fontSize: 10, weight: .bold)
         }
     }
     
@@ -114,24 +116,47 @@ class NotificationCell: UITableViewCell {
     }
     
     func convertToElapsedTime(notificationTime: String) -> String {
+        //알림 시간 쪼개기
         let splitNotificationTime = notificationTime.components(separatedBy: " ")
+        let notiDayArr =  split(target: splitNotificationTime[0], separateItem: "-")
+        let notiTimeArr = split(target: splitNotificationTime[1], separateItem: ":")
         
-        let notiYearMonthDay = splitNotificationTime[0]
-        let notiSplitDay = notiYearMonthDay.components(separatedBy: "-")
-        let notiYear = notiSplitDay[0]
-        let notiMonth = notiSplitDay[1]
-        let notiDay = notiSplitDay[2]
+        //현재 시간 쪼개기
+        date.locale = Locale(identifier: "ko_kr")
+        date.timeZone = TimeZone(abbreviation: "KST") // "2018-03-21 18:07:27"
+        date.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let currentTimeToKR = date.string(from: now)
+
+        let splitcurruntTime = "\(currentTimeToKR)".components(separatedBy: " ")
+        let currentDayArr = split(target: splitcurruntTime[0], separateItem: "-")
+        let currentTimeArr = split(target: splitcurruntTime[1], separateItem: ":")
         
-        let notiTime = splitNotificationTime[1]
-        let notiSplitTime = notiTime.components(separatedBy: ":")
-        let notiHour = notiSplitTime[0]
-        let notiMinute = notiSplitTime[1]
-        let notiSecond = notiSplitTime[2]
-        
-        _ = "\(Date())".components(separatedBy: " ")
-        
-        
-        return notiSecond
+        if currentDayArr[0] - notiDayArr[0] > 0 {
+//            년이 다름 (해가 바뀌는 순간엔 정확하진 않음 서버에서 timeStamp로 내려오지 않아 이정도에서 정리)
+            return "\(notiDayArr[0] - currentDayArr[0])년 전"
+        } else if currentDayArr[1] - notiDayArr[1] > 0 {
+//            년은 같고 달이 다름
+            return "\(currentDayArr[1] - notiDayArr[1])달 전"
+        } else if currentDayArr[2] - notiDayArr[2] > 0 {
+//            년,달 같고 일이 다름
+            return "\(currentDayArr[2] - notiDayArr[2])일 전"
+        } else if currentTimeArr[0] - notiTimeArr[0] > 0 {
+//            년,달,일 같고 시간이 다름
+            return "\(currentTimeArr[0] - notiTimeArr[0])시간 전"
+        } else if currentTimeArr[1] - notiTimeArr[1] > 0 {
+//            년,달,일,시간 같고 분이 다름
+            return "\(currentTimeArr[1] - notiTimeArr[1])분 전"
+        } else if currentTimeArr[2] - notiTimeArr[2] > 0 {
+//            년,달,일,시간,분이 같고 초가 다름
+            return "\(currentTimeArr[2] - notiTimeArr[2])초 전"
+        }
+        return ""
+    }
+    
+    func split(target: String, separateItem: String) -> [Int] {
+        let splitTarget = target.components(separatedBy: separateItem)
+        let result = splitTarget.map { Int($0)! }
+        return result
     }
     
     required init?(coder: NSCoder) {
