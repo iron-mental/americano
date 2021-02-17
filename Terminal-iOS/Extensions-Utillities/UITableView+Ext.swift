2085e643399d54320a1a9c888fd9a489ab1d06cd
jeongjaein <wodls96@gmail.com>
2021-02-17 18:53:34 +0900

    feat : UITableView+Ext 추가 (emptyView add, restore)

--- Terminal-iOS/Extensions-Utillities/UITableView+Ext.swift
+++ Terminal-iOS/Extensions-Utillities/UITableView+Ext.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2021 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+    func setEmptyView(type: EmptyViewCase) {
+        type.view.frame = CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height)
+        self.backgroundView = type.view
+        self.separatorStyle = .none
+    }
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
+}
