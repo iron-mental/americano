172b03bc8b9caa5a276700f04cfac593f04d33fc
jeongjaein <wodls96@gmail.com>
2021-02-23 19:15:37 +0900

    feat : LaunchInteractor 프로토콜 채택 및 구현

--- Terminal-iOS/LaunchView/LaunchInteractor.swift
+++ Terminal-iOS/LaunchView/LaunchInteractor.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+import SwiftKeychainWrapper
+
+class LaunchInteractor: LaunchInteractorInputProtocol {
+    weak var presenter: LaunchInteractorOutputProtocol?
+    var remoteDataManager: LaunchRemoteDataManagerInputProtocol?
+    
+    func getVersionCheck() {
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            remoteDataManager?.getVersionCheck(version: version)
+        }
+    }
+    
+    func refreshTokenCheck() {
+        if let userID = KeychainWrapper.standard.string(forKey: "userID") {
+            remoteDataManager?.getRefreshTokenValid(userID: userID)
+        } else {
+            presenter?.refreshTokenIsEmpty()
+        }
+    }
+}
+
+extension LaunchInteractor: LaunchRemoteDataManagerOutputProtocol {
+    func getVersionResult(result: BaseResponse<VersionResult>) {
+        switch result.result {
+        case true:
+            if result.data != nil {
+                switch VersionResultType(rawValue: result.data!.force) {
+                case .notRequired:
+                    refreshTokenCheck()
+                case .Recommended:
+                    presenter?.versionNeedUpdate(force: .Recommended)
+                case .Required:
+                    presenter?.versionNeedUpdate(force: .Required)
+                case .none, .some: break
+                }
+            }
+        case false: break
+        // 프레젠터 에러로 넘겨줌
+        }
+    }
+    
+    func getRefreshTokenResult(result: BaseResponse<UserInfo>) {
+        switch result.result {
+        case true:
+            presenter?.refreshTokenResult(result: result.result)
+        case false:
+            presenter?.refreshTokenResult(result: result.result)
+        }
+    }
+}
