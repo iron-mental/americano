13ece0e617298c853985a6ff694ab264f6299f52
jeongjaein <wodls96@gmail.com>
2021-02-24 01:59:17 +0900

    feat : ParticipantProfilePresenter 프로토콜 채택 및 구현

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ParticapateProfile/ParticipantProfilePresenter.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ParticapateProfile/ParticipantProfilePresenter.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+class ParticipantProfilePresenter: ParticipantProfilePresenterProtocol {
+    weak var view: ParticipantProfileViewProtocol?
+    var interactor: ParticipantProfileInteractorInputProtocol?
+    var wireFrame: ParticipantProfileWireFrameProtocol?
+    
+    func viewDidLoad() {
+        interactor?.getUserInfo()
+    }
+}
+
+extension ParticipantProfilePresenter: ParticipantProfileInteractorOutputProtocol {
+    func retriveUserInfo(result: Bool, userInfo: UserInfo) {
+        switch result {
+        case true:
+            LoadingRainbowCat.hide {
+                self.view?.showUserInfo(userInfo: userInfo)
+            }
+        case false:
+            print("ApplyUserDetailPresenter 에서 생긴 에러")
+        }
+    }
+    
+    func retriveProjectList(result: Bool, projectList: [Project]) {
+        switch result {
+        case true:
+            LoadingRainbowCat.hide {
+                self.view?.addProjectToStackView(project: projectList)
+            }
+        case false:
+            print("ApplyUserDetailPresenter")
+        }
+    }
+}
