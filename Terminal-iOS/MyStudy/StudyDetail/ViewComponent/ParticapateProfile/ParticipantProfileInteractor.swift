03ad641d4c8e327d88d2d89068c08d1a7ddbee9a
jeongjaein <wodls96@gmail.com>
2021-02-24 01:59:59 +0900

    feat : ParticipantProfileInteractor 프로토콜 채택 및 구현

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ParticapateProfile/ParticipantProfileInteractor.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/ParticapateProfile/ParticipantProfileInteractor.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+class ParticipantProfileInteractor: ParticipantProfileInteractorInputProtocol {
+    weak var presenter: ParticipantProfileInteractorOutputProtocol?
+    var remoteDataManager: ParticipantProfileRemoteDataManagerInputProtocol?
+    var userID: Int?
+    
+    func getUserInfo() {
+        guard let id = userID else { return }
+        remoteDataManager?.getUserInfo(userID: String(id))
+        remoteDataManager?.getProjectList(userID: String(id))
+    }
+}
+
+extension ParticipantProfileInteractor: ParticipantProfileRemoteDataManagerOutputProtocol {
+    func onUserInfoRetrieved(userInfo: BaseResponse<UserInfo>) {
+        switch userInfo.result {
+        case true:
+            if let data = userInfo.data {
+                presenter?.retriveUserInfo(result: userInfo.result, userInfo: data)
+            }
+        case false:
+            print("ApplyUserDetailInterator 에서 생긴 에러")
+        }
+    }
+    
+    func onProjectRetrieved(project: BaseResponse<[Project]>) {
+        switch project.result {
+        case true:
+            if let data = project.data {
+                presenter?.retriveProjectList(result: project.result, projectList: data)
+            }
+        case false:
+            print("ApplyUserDetailInterator 에서 생긴 에러")
+        }
+    }
+    
+}
