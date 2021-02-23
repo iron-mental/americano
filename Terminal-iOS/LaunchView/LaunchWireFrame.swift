5ef2b36005462e519bcc6ef17f6480a6106d3b53
jeongjaein <wodls96@gmail.com>
2021-02-23 19:16:24 +0900

    feat : LaunchWireFrame 프로토콜 채택 및 구현

--- Terminal-iOS/LaunchView/LaunchWireFrame.swift
+++ Terminal-iOS/LaunchView/LaunchWireFrame.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2021 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LaunchWireFrame: LaunchWireFrameProtocol {
+    
+    static func createLaunchModule() -> UIViewController {
+        let view = LaunchView()
+        let presenter: LaunchPresenterProtocol & LaunchInteractorOutputProtocol = LaunchPresenter()
+        let interactor: LaunchInteractorInputProtocol & LaunchRemoteDataManagerOutputProtocol = LaunchInteractor()
+        let remoteDataManager: LaunchRemoteDataManagerInputProtocol = LaunchRemoteDataManager()
+        let wireFrame: LaunchWireFrameProtocol = LaunchWireFrame()
+        
+        view.presenter = presenter
+        
+        presenter.view = view
+        presenter.wireFrame = wireFrame
+        presenter.interactor = interactor
+        
+        interactor.presenter = presenter
+        interactor.remoteDataManager = remoteDataManager
+        
+        remoteDataManager.interactor = interactor
+        
+        return view
+    }
+    func replaceRootViewToIntroView() {
+        guard let window = UIApplication.shared.windows.first else { return }
+//        window = UIWindow()
+//        let home = HomeView()
+    }
+    
+    func replaceRootViewToMainView() {
+        guard let window = UIApplication.shared.windows.first else { return }
+//        window = UIWindow()
+//        let main = ViewController()
+    }
+}
