fc2425835ee95b84e140a70336af7aade2b03675
jeongjaein <wodls96@gmail.com>
2021-02-23 20:28:25 +0900

    feat : LaunchView versionResult에 따른 플로우 작업

--- Terminal-iOS/LaunchView/LaunchView.swift
+++ Terminal-iOS/LaunchView/LaunchView.swift
@@ -15,11 +15,24 @@ class LaunchView: UIViewController {
         super.viewDidLoad()
         presenter?.viewDidLoad()
     }
+    
+    // MARK: @objc
+    @objc func updateButtonDidtap() {
+//        앱스토어로 가는 로직 구현부
+    }
+    
+    @objc func updateLaterButtonDidTap() {
+        presenter?.getRefreshTokenValid()
+    }
 }
 
 extension LaunchView: LaunchViewProtocol {
     func showVersionUpdateAlert(alertType: AlertType) {
         TerminalAlertMessage.show(controller: self, type: alertType)
+        TerminalAlertMessage.removeLeftButtonAction()
+        TerminalAlertMessage.removeRightButtonAction()
+        TerminalAlertMessage.getRightButton().addTarget(self, action: #selector(updateButtonDidtap), for: .touchUpInside)
+        TerminalAlertMessage.getLeftButton().addTarget(self, action: #selector(updateLaterButtonDidTap), for: .touchUpInside)
     }
     
     func showError(message: String) {
