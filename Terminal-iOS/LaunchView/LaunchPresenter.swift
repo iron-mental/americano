5326e960f5ffcc87234c40949055b3aa5dfd4bce
jeongjaein <wodls96@gmail.com>
2021-02-23 19:15:12 +0900

    feat : LaunchPresenter 프로토콜 채택 및 구현

--- Terminal-iOS/LaunchView/LaunchPresenter.swift
+++ Terminal-iOS/LaunchView/LaunchPresenter.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+class LaunchPresenter: LaunchPresenterProtocol {
+    weak var view: LaunchViewProtocol?
+    var interactor: LaunchInteractorInputProtocol?
+    var wireFrame: LaunchWireFrameProtocol?
+    
+    func viewDidLoad() {
+        interactor?.getVersionCheck()
+    }
+    func getRefreshTokenValid() {
+        interactor?.refreshTokenCheck()
+    }
+}
+
+extension LaunchPresenter: LaunchInteractorOutputProtocol {
+    func versionNeedUpdate(force: VersionResultType) {
+        switch force {
+        case .Recommended:
+            //새로운 얼럿(업데이트 추천 뷰)
+            view?.showVersionUpdateAlert(alertType: .AllowUserView)
+        case .Required:
+            //새로운 얼럿(업데이트 강제 뷰)
+            view?.showVersionUpdateAlert(alertType: .AllowUserView)
+        case .notRequired: break
+        }
+    }
+    
+    func refreshTokenIsEmpty() {
+        wireFrame?.replaceRootViewToIntroView()
+    }
+    
+    func refreshTokenResult(result: Bool) {
+        switch result {
+        case true:
+            wireFrame?.replaceRootViewToMainView()
+        case false:
+            wireFrame?.replaceRootViewToIntroView()
+        }
+    }
+}
