cfb30b36ef6463b5522035bbf7750c7c77804ed6
jeongjaein <wodls96@gmail.com>
2021-02-23 19:14:53 +0900

    feat : LaunchView 프로토콜 채택 및 구현

--- Terminal-iOS/LaunchView/LaunchView.swift
+++ Terminal-iOS/LaunchView/LaunchView.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2021 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LaunchView: UIViewController {
+    var presenter: LaunchPresenterProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter?.viewDidLoad()
+    }
+}
+
+extension LaunchView: LaunchViewProtocol {
+    func showVersionUpdateAlert(alertType: AlertType) {
+        TerminalAlertMessage.show(controller: self, type: alertType)
+    }
+    
+    func showError(message: String) {
+        //좀더 생각을 해보는 걸로 
+    }
+}
