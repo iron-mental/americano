73b5bd6d129a85133361f639e23af43bcc6570e8
jeongjaein <wodls96@gmail.com>
2021-02-23 19:16:03 +0900

    feat : LaunchRemoteDataManager 프로토콜 채택 및 구현

--- Terminal-iOS/LaunchView/LaunchRemoteDataManager.swift
+++ Terminal-iOS/LaunchView/LaunchRemoteDataManager.swift
@@ -7,3 +7,72 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+class LaunchRemoteDataManager: LaunchRemoteDataManagerInputProtocol {
+    weak var interactor: LaunchRemoteDataManagerOutputProtocol?
+    
+    func getVersionCheck(version: String) {
+        TerminalNetworkManager
+            .shared
+            .session
+            .request(TerminalRouter.versionCheck(version: version))
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    let data = "\(json)".data(using: .utf8)
+                    do {
+                        let result = try JSONDecoder().decode(BaseResponse<VersionResult>.self, from: data!)
+                        self.interactor?.getVersionResult(result: result)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                case .failure:
+                    if let data = response.data {
+                        do {
+                            let result = try JSONDecoder().decode(BaseResponse<VersionResult>.self, from: data)
+                            if result.message != nil {
+                                self.interactor?.getVersionResult(result: result)
+                            }
+                        } catch {
+                            
+                        }
+                    }
+                }
+            }
+    }
+    
+    func getRefreshTokenValid(userID: String) {
+        TerminalNetworkManager
+            .shared
+            .session
+            .request(TerminalRouter.userInfo(id: userID))
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    let data = "\(json)".data(using: .utf8)
+                    do {
+                        let result = try JSONDecoder().decode(BaseResponse<UserInfo>.self, from: data!)
+                        self.interactor?.getRefreshTokenResult(result: result)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                case .failure:
+                    if let data = response.data {
+                        do {
+                            let result = try JSONDecoder().decode(BaseResponse<UserInfo>.self, from: data)
+                            if result.message != nil {
+                                self.interactor?.getRefreshTokenResult(result: result)
+                            }
+                        } catch {
+                            
+                        }
+                    }
+                }
+            }
+    }
+}
