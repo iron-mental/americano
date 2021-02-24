46681f1413bea8fbe202efe91a7227b8cd3c0435
jeongjaein <wodls96@gmail.com>
2021-02-24 20:49:55 +0900

    feat : NotiCell 토글 제거, guidLabel 추가

--- Terminal-iOS/Set/Set/Component/NotiCell.swift
+++ Terminal-iOS/Set/Set/Component/NotiCell.swift
@@ -11,38 +11,31 @@ import SwiftKeychainWrapper
 
 class NotiCell: DefaultCell {
     static let notiCellId = "notiCell"
-    lazy var notiToggle = UISwitch()
+    lazy var state = UILabel()
     var result = false
     
     override func attribute() {
         super.attribute()
-        accessoryView = notiToggle
         rightLabel.do {
-            $0.isHidden = true
-        }
-        notiToggle.do { toggle in
-            toggle.addTarget(self, action: #selector(notiToggleDidTap(_: )), for: .touchUpInside)
+            $0.dynamicFont(fontSize: 15, weight: .regular)
             getNotificationSetting { result in
                 DispatchQueue.main.async {
-                    toggle.setOn(result, animated: true)
+                    self.rightLabel.text = result ? "ON" : "OFF"
+                    self.rightLabel.textColor = result ? UIColor.appColor(.mainColor) : .systemGray
                 }
             }
         }
+    }
+    
+    override func layout() {
+        super.layout()
+        rightLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Terminal.convertWidth(value: 40)).isActive = true
     }
     
     func getNotificationSetting(completion: @escaping (Bool) -> Void) {
         UNUserNotificationCenter.current().getNotificationSettings { [self] in
             self.result = $0.notificationCenterSetting.rawValue == 2
             completion(result)
-        }
-    }
-    
-    @objc func notiToggleDidTap(_ sender: UISwitch) {
-        if let bundleIdentifier = Bundle.main.bundleIdentifier,
-           let appSettings = URL(string: UIApplication.openSettingsURLString + bundleIdentifier) {
-            if UIApplication.shared.canOpenURL(appSettings) {
-                UIApplication.shared.open(appSettings)
-            }
         }
     }
 }
