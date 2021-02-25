d79efb48dccc95899dd44c034f7de66aa54892a5
s-doris <swdoriz@gmail.com>
2021-02-26 02:02:47 +0900

    feat : FindPassword init

---
+++ Terminal-iOS/FindPassword/FindPasswordRemoteDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  FindPasswordRemoteDataManager.swift
+//  Terminal-iOS
+//
+//  Created by once on 2021/02/26.
+//  Copyright © 2021 정재인. All rights reserved.
+//
+
+import Foundation
+
+final class FindPasswordRemoteDataManager {
+    
+}
---
+++ Terminal-iOS/FindPassword/FindPasswordView.swift
@@ -0,0 +1,17 @@
+//
+//  FindPasswordView.swift
+//  Terminal-iOS
+//
+//  Created by once on 2021/02/26.
+//  Copyright © 2021 정재인. All rights reserved.
+//
+
+import UIKit
+
+final class FindPasswordView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
