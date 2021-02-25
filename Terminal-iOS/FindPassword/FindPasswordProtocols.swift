8a08e6c22d1026c3c02049859c04503cbf9e061e
s-doris <swdoriz@gmail.com>
2021-02-26 02:12:36 +0900

    feat : FindPasswordProtocols init

--- Terminal-iOS/FindPassword/FindPasswordProtocols.swift
+++ Terminal-iOS/FindPassword/FindPasswordProtocols.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2021 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol FindPasswordViewProtocol: class {
+    var presenter: FindPasswordPresenterProtocol? { get set }
+    
+    // PRESENTER -> VIEW
+}
+
+protocol FindPasswordWireFrameProtocol: class {
+    static func createModule() -> UIViewController
+}
+
+protocol FindPasswordPresenterProtocol: class {
+    var view: FindPasswordViewProtocol? { get set }
+    var interactor: FindPasswordInteractorInputProtocol? { get set }
+    var wireFrame: FindPasswordWireFrameProtocol? { get set }
+    
+    // VIEW -> PRESENTER
+}
+
+protocol FindPasswordInteractorInputProtocol: class {
+    var presenter: FindPasswordInteractorOutputProtocol? { get set }
+    var remoteDataManager: FindPasswordRemoteDataManagerInputProtocol? { get set }
+    
+    // PRESENTER -> INTERACTOR
+}
+
+protocol FindPasswordInteractorOutputProtocol: class {
+    
+    // INTERACTOR -> PRESENTER
+}
+
+protocol FindPasswordRemoteDataManagerInputProtocol: class {
+    var interactor: FindPasswordRemoteDataManagerOutputProtocol? { get set }
+    
+    // INTERACTOR -> REMOTEDATAMANAGER
+}
+
+protocol FindPasswordRemoteDataManagerOutputProtocol: class {
+    
+    // REMOTEDATAMANAGER -> INTERACTOR
+}
