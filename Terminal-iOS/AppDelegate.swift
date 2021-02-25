51ed044d1caff2753d052ecc92028121140d1f23
jeongjaein <wodls96@gmail.com>
2021-02-25 18:47:47 +0900

    feat : AppDelegate - push 입장 로직 추가

--- Terminal-iOS/AppDelegate.swift
+++ Terminal-iOS/AppDelegate.swift
@@ -26,22 +26,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        // 임시 슈가 코드
+        print(KeychainWrapper.standard.string(forKey: "refreshToken"))
+        print(KeychainWrapper.standard.string(forKey: "accessToken"))
+        
         // firebase 연동
         FirebaseApp.configure()
         
-        if let notification = launchOptions?[.remoteNotification] as? [String: AnyObject] {
-            if let studyID = notification["study_id"] as? String,
-               let pushEvent = notification["pushEvent"] as? String {
-                self.studyID = studyID
-                self.pushEvent = AlarmType(rawValue: pushEvent)
-            }
-        }
-        
-        window = UIWindow()
-        let launchView = LaunchWireFrame.createLaunchModule()
-        window?.rootViewController = launchView
-        window?.makeKeyAndVisible()
-        
+        // Noti Request
         let center = UNUserNotificationCenter.current()
         center.delegate = self
         center.requestAuthorization(options: [.alert, .badge, .sound]) { _, error in
@@ -53,8 +45,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             }
         }
         
-        print(KeychainWrapper.standard.string(forKey: "refreshToken"))
-        print(KeychainWrapper.standard.string(forKey: "accessToken"))
+        if let notification = launchOptions?[.remoteNotification] as? [String: AnyObject] {
+            if let studyID = notification["study_id"] as? String,
+               let pushEvent = notification["pushEvent"] as? String {
+                self.studyID = studyID
+                self.pushEvent = AlarmType(rawValue: pushEvent)
+            }
+        } else {
+            window = UIWindow()
+            let launchView = LaunchWireFrame.createLaunchModule()
+            window?.rootViewController = launchView
+            window?.makeKeyAndVisible()
+        }
         return true
     }
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
@@ -102,10 +104,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 //        이부분에서 런치뷰가 이미 깔려있기에 캐스팅이 되질 않고 특정뷰로 가지않음
 //        그렇다고 버전체크와 리프레시토큰을 확인 안할 수 없으므로 그걸 런치뷰에서 다할 것이냐 아니면 앱델리게이트에서 할 것이냐 정하면 되는데
 //        뭐가 좀 더 합리적인지 체크해보자
-        if let tabVC = self.window?.rootViewController as? UITabBarController,
-           let navVC = tabVC.selectedViewController as? UINavigationController {
-            navVC.pushViewController(goView!, animated: true)
-        }
+        
+        //이건 처음 들어왔을 때 만
+        window = UIWindow()
+        //여기에 스터디 아이디랑 알림 스타일 넘겨줘야됨
+        let launchView = LaunchWireFrame.createLaunchModule(studyID: studyID, pushEvent: pushEvent)
+        window?.rootViewController = launchView
+        window?.makeKeyAndVisible()
+        
+        //이건 foreground 일 때
+//        if let tabVC = self.window?.rootViewController as? UITabBarController,
+//           let navVC = tabVC.selectedViewController as? UINavigationController {
+//            navVC.pushViewController(goView!, animated: true)
+//        }
         completionHandler()
     }
     
