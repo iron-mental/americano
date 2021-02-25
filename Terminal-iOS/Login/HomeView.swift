de002bcd3b0f5fdd214f29928879d12ee8517a01
s-doris <swdoriz@gmail.com>
2021-02-26 02:49:23 +0900

    feat : findPWButton layout and attribute init

--- Terminal-iOS/Login/HomeView.swift
+++ Terminal-iOS/Login/HomeView.swift
@@ -12,6 +12,7 @@ import SwiftKeychainWrapper
 class HomeView: UIViewController {
     var loginButton = UIButton()
     var signUpButton = UIButton()
+    var findPWButton = ResizableButton()
     var mainImage = UIImageView()
     
     override func viewWillAppear(_ animated: Bool) {
@@ -21,7 +22,6 @@ class HomeView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
         attribute()
         layout()
     }
@@ -30,10 +30,8 @@ class HomeView: UIViewController {
         self.do {
             $0.navigationController?.navigationBar.shadowImage = UIImage()
             $0.navigationController?.navigationBar.isTranslucent = false
-            $0.navigationController?.navigationBar.backgroundColor = UIColor.white
-        }
-        view.do {
-            $0.backgroundColor = UIColor.appColor(.terminalBackground)
+            $0.navigationController?.navigationBar.backgroundColor = .white
+            $0.view.backgroundColor = .appColor(.terminalBackground)
         }
         loginButton.do {
             $0.setTitle("로그인", for: .normal)
@@ -48,6 +46,13 @@ class HomeView: UIViewController {
             $0.layer.cornerRadius = 10
             $0.layer.masksToBounds = true
         }
+        findPWButton.do {
+            $0.setTitle("로그인에 문제가 있으신가요?", for: .normal)
+            $0.setTitleColor(.appColor(.mainColor), for: .normal)
+            $0.titleLabel?.dynamicFont(fontSize: 12, weight: .medium)
+            $0.backgroundColor = .darkGray
+            $0.addTarget(self, action: #selector(goFindPassword), for: .touchUpInside)
+        }
         mainImage.do {
             $0.image = UIImage(systemName: "terminal.fill")?.withConfiguration(UIImage.SymbolConfiguration(weight: .light))
             $0.tintColor = .white
@@ -56,24 +61,34 @@ class HomeView: UIViewController {
     }
     
     func layout() {
-        view.addSubview(loginButton)
-        view.addSubview(signUpButton)
-        view.addSubview(mainImage)
+        [loginButton, signUpButton, findPWButton, mainImage].forEach { view.addSubview($0) }
         
         loginButton.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
-            $0.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+            $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                    constant: 10).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                                         constant: -10).isActive = true
             $0.widthAnchor.constraint(equalToConstant: 100).isActive = true
             $0.heightAnchor.constraint(equalToConstant: 50).isActive = true
         }
         signUpButton.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: Terminal.convertWidth(value: -15) ).isActive = true
-            $0.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Terminal.convertWidth(value: 15) ).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                                         constant: Terminal.convertWidth(value: -15) ).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                                        constant: Terminal.convertWidth(value: 15) ).isActive = true
             $0.heightAnchor.constraint(equalToConstant: Terminal.convertHeight(value: 50)).isActive = true
-            $0.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: Terminal.convertHeight(value: -20)).isActive = true
+            $0.bottomAnchor.constraint(equalTo: self.findPWButton.topAnchor,
+                                       constant: Terminal.convertHeight(value: -5)).isActive = true
             $0.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        }
+        findPWButton.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.heightAnchor.constraint(equalToConstant: Terminal.convertHeight(value: 40)).isActive = true
+            $0.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            $0.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                       constant: Terminal.convertHeight(value: -5)).isActive = true
         }
         mainImage.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
@@ -82,6 +97,10 @@ class HomeView: UIViewController {
             $0.widthAnchor.constraint(equalToConstant: 300).isActive = true
             $0.heightAnchor.constraint(equalToConstant: 130).isActive = true
         }
+    }
+    
+    @objc func goFindPassword() {
+        
     }
     
     @objc func goLogin() {
