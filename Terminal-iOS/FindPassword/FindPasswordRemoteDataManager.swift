abc5beaf45192fa238789fd602c5867537dd0e4b
s-doris <swdoriz@gmail.com>
2021-02-26 19:32:29 +0900

    feat : FindPassword network request and response init

--- Terminal-iOS/FindPassword/FindPasswordRemoteDataManager.swift
+++ Terminal-iOS/FindPassword/FindPasswordRemoteDataManager.swift
@@ -7,8 +7,36 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 final class FindPasswordRemoteDataManager: FindPasswordRemoteDataManagerInputProtocol {
     var interactor: FindPasswordRemoteDataManagerOutputProtocol?
     
+    func resetPassword(email: String) {
+        let url = API.BASE_URL+"user/reset-password/\(email)"
+        
+        AF.request(url,
+                   method: .post)
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    do {
+                        let result = try JSONDecoder().decode(BaseResponse<Bool>.self, from: data)
+                        self.interactor?.resetResponse(result: result)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                case .failure:
+                    if let data = response.data {
+                        do {
+                            let result = try JSONDecoder().decode(BaseResponse<Bool>.self, from: data)
+                            self.interactor?.resetResponse(result: result)
+                        } catch {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
+            }
+    }
 }
