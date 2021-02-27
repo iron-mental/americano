e8aa9d0373a5897c9a1fed1d26f960eae676f77a
s-doris <swdoriz@gmail.com>
2021-02-28 00:01:37 +0900

    refactor : 프로필 sns 존재 여부에 따라서 alpha값 조정

--- Terminal-iOS/Set/MyProfile/Component/ProfileSNSView.swift
+++ Terminal-iOS/Set/MyProfile/Component/ProfileSNSView.swift
@@ -48,41 +48,26 @@ class ProfileSNSView: BaseSNSView {
     }
     
     override func addstack(snsList: [String: String]) {
-        /// 추가된 SNS 갯수
-        var count = 0
-        
         self.snsStack.removeAllArrangedSubviews()
         
-        if snsList["github"] != nil {
-            self.snsStack.addArrangedSubview(self.github)
-            count += 1
+        if snsList["github"]!.isEmpty {
+            github.alpha = 0.3
         }
         
-        if snsList["linkedin"] != nil {
-            self.snsStack.addArrangedSubview(self.linkedin)
-            count += 1
+        if snsList["linkedin"]!.isEmpty {
+            linkedin.alpha = 0.3
         }
         
-        if snsList["web"] != nil {
-            self.snsStack.addArrangedSubview(self.web)
-            count += 1
+        if snsList["web"]!.isEmpty {
+            web.alpha = 0.3
         }
         
-        switch count {
-        case 1:
-            self.firstWidth.isActive = true
-            self.secondWidth.isActive = false
-            self.thirdWidth.isActive = false
-        case 2:
-            self.firstWidth.isActive = false
-            self.secondWidth.isActive = true
-            self.thirdWidth.isActive = false
-        case 3:
-            self.firstWidth.isActive = false
-            self.secondWidth.isActive = false
-            self.thirdWidth.isActive = true
-        default:
-            break
-        }
+        self.snsStack.addArrangedSubview(self.github)
+        self.snsStack.addArrangedSubview(self.linkedin)
+        self.snsStack.addArrangedSubview(self.web)
+        
+        self.firstWidth.isActive = false
+        self.secondWidth.isActive = false
+        self.thirdWidth.isActive = true
     }
 }
