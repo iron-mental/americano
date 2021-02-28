730c118cfb52e9f5a17630143d489e9552dab73b
s-doris <swdoriz@gmail.com>
2021-02-28 20:55:11 +0900

    fix : ApplyList selectionStyle 수정

--- Terminal-iOS/BaseComponent/ApplyListCell.swift
+++ Terminal-iOS/BaseComponent/ApplyListCell.swift
@@ -16,6 +16,7 @@ class ApplyListCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
         attribute()
         layout()
     }
