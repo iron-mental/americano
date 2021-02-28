50db25d24ee5d1bf13cda2ac684a27a6e9c445f3
s-doris <swdoriz@gmail.com>
2021-03-01 02:42:55 +0900

    feat : ApplyMessageView 스터디 신청 유저의 메세지를 보여주기 위한 UIView

--- Terminal-iOS/Set/MyProfile/Component/ApplyMessageView.swift
+++ Terminal-iOS/Set/MyProfile/Component/ApplyMessageView.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2021 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ApplyMessageView: UIView {
+    let message = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        attribute()
+        layout()
+    }
+    
+    func attribute() {
+        self.do {
+            $0.layer.borderWidth = 0.1
+            $0.layer.borderColor = UIColor.gray.cgColor
+        }
+        self.message.do {
+            $0.textColor = .white
+            $0.dynamicFont(fontSize: 15, weight: .medium)
+            $0.textColor = .appColor(.profileTextColor)
+        }
+    }
+    func layout() {
+        self.addSubview(message)
+        
+        self.message.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+            $0.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
