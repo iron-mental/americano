474f5110ffd29fd32243b2e40562bab32e0996a4
jeongjaein <wodls96@gmail.com>
2021-03-03 17:05:42 +0900

    feat : applystudy - 신청메세지작성칸 UITextField -> UITextView 변경

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/StudyDetail/StudyDetailInteractor.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/StudyDetail/StudyDetailInteractor.swift
@@ -18,7 +18,12 @@ final class StudyDetailInteractor: StudyDetailInteractorInputProtocol {
     }
     
     func postStudyJoin(studyID: Int, message: String) {
-        remoteDatamanager?.postStudyJoin(studyID: studyID, message: message)
+        if message == "ex) 열심히 공부할 자신 있습니다!!" ||
+            message.isEmpty {
+            presenter?.studyJoinResult(result: false, message: "공백은 허용되지 않습니다")
+        } else {
+            remoteDatamanager?.postStudyJoin(studyID: studyID, message: message)
+        }
     }
     
     func postReportStudy(studyID: Int, reportMessage: String) {
--- Terminal-iOS/StaticComponent/StudyApplyMessageView.swift
+++ Terminal-iOS/StaticComponent/StudyApplyMessageView.swift
@@ -17,7 +17,7 @@ class StudyApplyMessageView: AlertBaseUIView {
     var type: StudyApplyMessageType = .apply
     var applyTitleLabel = UILabel()
     var applyGuideLabel = UILabel()
-    var editMessageTextField = UITextField()
+    var editMessageTextView = UITextView()
     
     init(type: StudyApplyMessageType) {
         super.init()
@@ -37,16 +37,21 @@ class StudyApplyMessageView: AlertBaseUIView {
             $0.dynamicFont(fontSize: 13, weight: .bold)
             $0.text = type == .apply ? "가입 인사를 작성해보세요" : "수정할 메세지를 작성하세요"
         }
-        editMessageTextField.do {
-            $0.dynamicFont(fontSize: 13, weight: .regular)
-            $0.placeholder = "스터디 참여를 희망합니다."
+        editMessageTextView.do {
+            $0.dynamicFont(size: 10, weight: .regular)
+            $0.delegate = self
+            $0.backgroundColor = .systemGray5
+            $0.layer.cornerRadius = 6
+            $0.layer.masksToBounds = true
+            $0.text = "ex) 열심히 공부할 자신 있습니다!!"
+            $0.textColor = .systemGray2
         }
     }
     
     override func layout() {
         super.layout()
         
-        [bottomBar, applyTitleLabel, applyGuideLabel, editMessageTextField].forEach { addSubview($0) }
+        [bottomBar, applyTitleLabel, applyGuideLabel, editMessageTextView].forEach { addSubview($0) }
         
         applyTitleLabel.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
@@ -58,15 +63,24 @@ class StudyApplyMessageView: AlertBaseUIView {
             $0.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Terminal.convertWidth(value: 20)).isActive = true
             $0.topAnchor.constraint(equalTo: applyTitleLabel.bottomAnchor, constant: 16).isActive = true
         }
-        editMessageTextField.do {
+        editMessageTextView.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Terminal.convertWidth(value: 20)).isActive = true
-            $0.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-            $0.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Terminal.convertWidth(value: 20)).isActive = true
+            $0.topAnchor.constraint(equalTo: applyGuideLabel.bottomAnchor, constant: Terminal.convertHeight(value: 15)).isActive = true
+            $0.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Terminal.convertWidth(value: 15)).isActive = true
+            $0.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Terminal.convertWidth(value: 15)).isActive = true
+            $0.bottomAnchor.constraint(equalTo: completeButton.topAnchor, constant: -Terminal.convertHeight(value: 15)).isActive = true
         }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension StudyApplyMessageView: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.dynamicFont(size: 12, weight: .regular)
+        textView.textColor = .white
+        textView.text = ""
     }
 }
