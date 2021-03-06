442a157aa92d1fb816d399b8b5f90e70a49761f0
s-doris <swdoriz@gmail.com>
2021-03-06 18:04:34 +0900

    feat : 라이센스 디테일 뷰 추가

--- Terminal-iOS/Set/Licenses/LicenseDetailView.swift
+++ Terminal-iOS/Set/Licenses/LicenseDetailView.swift
@@ -9,22 +9,77 @@
 import UIKit
 
 class LicenseDetailView: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    let library: String
+    let descript = UITextView()
+    
+    init(library: String) {
+        self.library = library
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        attribute()
+        layout()
+        
+        if let descript = licenseDescript(library: self.library) {
+            self.descript.text = descript
+        }
     }
-    */
-
+    
+    private func attribute() {
+        self.do {
+            $0.navigationItem.largeTitleDisplayMode = .never
+        }
+        self.descript.do {
+            $0.backgroundColor = .appColor(.terminalBackground)
+            $0.textColor = .white
+            $0.isEditable = false
+            $0.isUserInteractionEnabled = true
+            $0.dataDetectorTypes = .link
+        }
+    }
+    
+    private func layout() {
+        self.view.addSubview(self.descript)
+        self.descript.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+            $0.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+            $0.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        }
+    }
+    
+    func licenseDescript(library: String) -> String? {
+        let license = LibraryLicense()
+        switch library {
+        case "Alamofire":
+            return license.alamofire
+        case "Firebase/Analytics":
+            return license.firebase
+        case "Firebase/Crashlytics":
+            return license.firebase
+        case "Kingfisher":
+            return license.kingfisher
+        case "lottie-ios":
+            return license.lottie
+        case "NMapsMap":
+            return license.nMapsMap
+        case "SwiftyJSON":
+            return license.swiftyJSON
+        case "SwiftLint":
+            return license.swiftLint
+        case "SwiftKeychainWrapper":
+            return license.swiftKeychainWrapper
+        case "Then":
+            return license.then
+        default:
+            return nil
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
