a17cc7f4d285c4fb232864340f5611da6f890de0
jeongjaein <wodls96@gmail.com>
2021-03-07 17:10:48 +0900

    feat : ApplyUserListEmptyView - ipad 화면 대응

--- Terminal-iOS/EmptyViews/ApplyUserListEmptyView.swift
+++ Terminal-iOS/EmptyViews/ApplyUserListEmptyView.swift
@@ -27,7 +27,12 @@ final class ApplyUserListEmptyView: BaseEmptyView {
         }
         self.guideLabel.do {
             guideLabelTopLayout?.isActive = false
-            $0.topAnchor.constraint(equalTo: self.iconImageView.bottomAnchor, constant: Terminal.convertHeight(value: 100)).isActive = true
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                $0.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: Terminal.convertHeight(value: 200)).isActive = true
+            } else {
+                $0.topAnchor.constraint(equalTo: self.iconImageView.bottomAnchor, constant: Terminal.convertHeight(value: 100)).isActive = true
+            }
         }
+        
     }
 }
