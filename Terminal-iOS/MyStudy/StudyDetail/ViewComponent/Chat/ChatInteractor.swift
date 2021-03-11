ebd7134a776e50ea12e706410a27fd6c98e4f844
jeongjaein <wodls96@gmail.com>
2021-03-11 21:45:22 +0900

    feat : ChatInteractor - chat 관련 로직 구현

--- Terminal-iOS/MyStudy/StudyDetail/ViewComponent/Chat/ChatInteractor.swift
+++ Terminal-iOS/MyStudy/StudyDetail/ViewComponent/Chat/ChatInteractor.swift
@@ -13,58 +13,57 @@ class ChatInteractor: ChatInteractorProtocol {
     var remoteDataManager: ChatRemoteDataManagerProtocol?
     var localDataManager: ChatLocalDataManagerProtocol?
     var studyID: Int?
-    var lastLocalChat: [String] = []
-    var receiveFromSocketChat: [String] = []
-    var mergeChatFlag = false
+    var lastLocalChat: [Chat] = []
+    var receiveFromSocketChat: [Chat] = []
     var lastTimeStamp: Int?
+    var mergeChatFromSocketFlag = false
     
     func connectSocket() {
-//        if let localChat = 코어데이터 가지고온 후 localChat에 넣어주기
-//        remoteDataManager?.socketConnect(studyID: studyID!)
+        getLastLocalChat {
+            self.remoteDataManager?.socketConnect(studyID: self.studyID!)
+        }
     }
     func emit(message: String) {
-        let test = Chat(studyID: studyID!,
-                        nickname: "재인",
-                        message: message,
-                        date: "testDate")
-        CoreDataManager.shared.saveChatInfo(studyID: studyID!, chatList: [test])
         remoteDataManager?.emit(message: message)
-//        CoreDataManager.shared.getCurrentChatInfo(studyID: studyID!)
     }
     func disconnectSocket() {
         remoteDataManager?.disconnectSocket()
     }
-    func getLastLocalChat() {
-        //코어 데이터로부터 과거에 저장한 채팅을 가지고옴
-        //        lastLocalChat = CoreDataManager.get
+    func getLastLocalChat(_: @escaping () -> Void ) {
+        //로컬 챗 세팅
+        lastLocalChat = CoreDataManager.shared.getCurrentChatInfo(studyID: studyID!)
     }
     func receiveMessage(message: Chat) {
-        //소켓을 통해 들어온 메세지는 그저 이곳에 저장만
-//        receiveFromSocketChat.append(message)
+        //챗 from socket
+        receiveFromSocketChat.append(message)
         arrangeChat()
     }
-    func receiveLastChat(lastRemoteChat: [String]) {
-        //리모트로부터 과거에 저장된 채팅을 가지고옴
-        //합쳐주고
-        _ = lastLocalChat + lastRemoteChat
-        //프레젠터로 넘겨주고
-        //        presenter.getLastChatResult(lastChat: wholeLastChat)
-        //기준이 될 라스트 스탬프 할당 해준뒤
-        //        lastTimeStamp = lastRemoteChat.last.timeStamp
-        // 스위치 온!! 하면 뷰에 arrangeChat이 먼저도착할지 어떻게알아 확실하게 뷰에 다뿌려졌으면 다시 뷰->프레젠터->mergeChatFlag on해서 뷰로 넘겨주는걸로 하자 ㅇ.ㅇ
-//        mergeChatFlag = true
+    func receiveLastChat(lastRemoteChat: [Chat]) {
+        //기준이 될 라스트 타임스탬프 할당
+        lastTimeStamp = lastRemoteChat.last?.date
+        //로컬에 가지고온 애를 할당하는거 추가
+        //      CoreDataManager save chat 블라블라~
+        //로컬 + 리모트 채팅을 프레젠터로 패스
+        presenter?.getLastChatResult(lastChat: lastLocalChat + lastRemoteChat)
+    }
+    func mergeChatFromSocket() {
+        //중복되었다면 지워주고
+        if lastTimeStamp! >= receiveFromSocketChat.first!.date {
+            receiveFromSocketChat.removeFirst()
+        }
+        //소켓으로 떨어진 채팅 처리 시작
+        mergeChatFromSocketFlag = true
+        arrangeChat()
     }
     func arrangeChat() {
-        //스위치도 켜져있고 소켓으로 넘어온 메세지가 있다면
-//        while mergeChatFlag
-//                && !receiveFromSocketChat.isEmpty{
-//            //타임 스탬프보다 전 메세지면 그걸 그냥 지워
-//            if lastTimeStamp! >= receiveFromSocketChat.first.timeStamp {
-//                receiveFromSocketChat.removeFirst()
-//            } else if {
-//            //타임스탬프 기준보다 나중 메세지면 뷰로 보내자
-//                presenter?.showReceiveMessage(message: receiveFromSocketChat.first)
-//            }
-//        }
+        var arragedChatList: [Chat] = []
+        while mergeChatFromSocketFlag
+                && !receiveFromSocketChat.isEmpty{
+            arragedChatList.append(receiveFromSocketChat.first!)
+            receiveFromSocketChat.removeFirst()
+        }
+        if !arragedChatList.isEmpty {
+            presenter?.arrangedChatFromChat(chat: arragedChatList)
+        }
     }
 }
