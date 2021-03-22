a257b9fbda50a984bf08388ce9df39d94add3128
jeongjaein <wodls96@gmail.com>
2021-03-22 18:44:00 +0900

    feat : chat -

---
+++ CoreChatInfo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CoreChatInfo+CoreDataClass.swift
+//  
+//
+//  Created by 정재인 on 2021/03/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CoreChatInfo)
+public class CoreChatInfo: NSManagedObject {
+
+}
